e18352e052ac75f942c006fb6297cff76f5211bf
Joshua Cranmer <Pidgeot18@gmail.com>
2011-06-20 16:34:09 -0700

    Minor fixes, see Issue #1 on github.

--- www/js/search.js
+++ www/js/search.js
@@ -9,21 +9,21 @@ function doSearch(id) {
   for (var i = 0; i < args.length; i++) {
     var arg = args[i];
 
-    if (/^path:.+/(arg))
+    if (/^path:.+/.exec(arg))
       url += "&path=" + encodeURIComponent(/^path:(.+)/.exec(arg).slice(1,2));
-    else if (/^ext:.+/(arg))
+    else if (/^ext:.+/.exec(arg))
       url += "&ext=" + encodeURIComponent(/^ext:(.+)/.exec(arg).slice(1,2));
-    else if (/^type:.+/(arg))
+    else if (/^type:.+/.exec(arg))
       url += "&type=" + encodeURIComponent(/^type:(.+)/.exec(arg).slice(1,2));
-    else if (/^member:.+/(arg))
+    else if (/^member:.+/.exec(arg))
       url += "&member=" + encodeURIComponent(/^member:(.+)/.exec(arg).slice(1,2));
-    else if (/^derived:.+/(arg))
+    else if (/^derived:.+/.exec(arg))
       url += "&derived=" + encodeURIComponent(/^derived:(.+)/.exec(arg).slice(1,2));
-    else if (/^callers:.+/(arg))
+    else if (/^callers:.+/.exec(arg))
       url += "&callers=" + encodeURIComponent(/^callers:(.+)/.exec(arg).slice(1,2));
-    else if (/^macro:.+/(arg))
+    else if (/^macro:.+/.exec(arg))
       url += "&macro=" + encodeURIComponent(/^macro:(.+)/.exec(arg).slice(1,2));
-    else if (/^warnings:.*/(arg)) {
+    else if (/^warnings:.*/.exec(arg)) {
       var warnings = /^warnings:(.*)/.exec(arg).slice(1,2);
       
       // see if user did warnings:<nothing>, meaning "show all warnings"
@@ -39,14 +39,13 @@ function doSearch(id) {
 
   if (string.length > 0) {
     string = string.substring(0, string.length-1);
-    if (/^\/.+\/$/(string)) {
+    if (/^\/.+\/$/.exec(string)) {
       string = string.substring(1, string.length-1);
       url += "&regexp=on";
     }
     url += "&string=" + encodeURIComponent(string);
   }
 
-  url = url.replace(/\&/g, '&amp;');
   window.location = url;
   return false;
 }
