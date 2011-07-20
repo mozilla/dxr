ff49e144bc94682b40fb1d403d23aed2027d2749
Joshua Cranmer <Pidgeot18@gmail.com>
2011-07-20 15:38:15 -0700

    The patch I based this on appears to be subtly wrong.

--- xref-tools/moztools/myrules.mk
+++ xref-tools/moztools/myrules.mk
@@ -6,7 +6,7 @@ $(XPIDL_GEN_DIR)/%.idlcsv: %.idl $(DXRSRC)/xref-tools/moztools/idl_xref.py
 	$(PYTHON) $(MOZILLA_DIR)/config/pythonpath.py \
 		-I$(MOZILLA_DIR)/other-licenses/ply \
 		-I$(MOZILLA_DIR)/xpcom/idl-parser \
-		$(DXRSRC)/xref-tools/moztools/idl_xref.py --cachedir=$(MOZILLA_DIR)/xpcom/idl-parser -I $(DIST)/idl $(_VPATH_SRCS) > $@
+		$(DXRSRC)/xref-tools/moztools/idl_xref.py --cachedir=$(MOZILLA_DIR)/xpcom/idl-parser $(XPIDL_FLAGS) $(_VPATH_SRCS) > $@
 
 export:: $(patsubst %.idl,$(XPIDL_GEN_DIR)/%.idlcsv, $(XPIDLSRCS))
 endif
