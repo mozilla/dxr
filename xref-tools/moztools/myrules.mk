d4fa592c93fdbcce7af90e6d1f67fdb47c95b85e
Joshua Cranmer <Pidgeot18@gmail.com>
2011-07-22 15:12:54 -0700

    The comm-central build system is more complicated than I thought.

--- xref-tools/moztools/myrules.mk
+++ xref-tools/moztools/myrules.mk
@@ -1,12 +1,10 @@
 ifneq ($(XPIDLSRCS),)
-ifndef $(MOZILLA_DIR)
-  MOZILLA_DIR := $(topsrcdir)
-endif
+xpidldir := $(if $(wildcard $(topsrcdir)/mozilla),$(topsrcdir)/mozilla,$(topsrcdir))
 $(XPIDL_GEN_DIR)/%.idlcsv: %.idl $(DXRSRC)/xref-tools/moztools/idl_xref.py
-	$(PYTHON) $(MOZILLA_DIR)/config/pythonpath.py \
-		-I$(MOZILLA_DIR)/other-licenses/ply \
-		-I$(MOZILLA_DIR)/xpcom/idl-parser \
-		$(DXRSRC)/xref-tools/moztools/idl_xref.py --cachedir=$(MOZILLA_DIR)/xpcom/idl-parser $(XPIDL_FLAGS) $(_VPATH_SRCS) > $@
+	$(PYTHON) $(xpidldir)/config/pythonpath.py \
+		-I$(xpidldir)/other-licenses/ply \
+		-I$(xpidldir)/xpcom/idl-parser \
+		$(DXRSRC)/xref-tools/moztools/idl_xref.py --cachedir=$(xpidldir)/xpcom/idl-parser $(XPIDL_FLAGS) $(_VPATH_SRCS) > $@
 
 export:: $(patsubst %.idl,$(XPIDL_GEN_DIR)/%.idlcsv, $(XPIDLSRCS))
 endif
