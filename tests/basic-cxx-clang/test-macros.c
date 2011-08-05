325f902f4340e6824f7b1341d111e7a3b3a34b36
Joshua Cranmer <Pidgeot18@gmail.com>
2011-08-05 15:24:44 -0700

    Add in a basic test framework for DXR.

---
+++ tests/basic-cxx-clang/test-macros.c
@@ -0,0 +1,4 @@
+/* Tests to make sure that macros are handled correctly. */
+
+#define MACRO1(a) a
+#define MACRO2 (a)
---
+++ tests/run-test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Bring us to this directory.
+cd $(dirname $(readlink -f $0))
+
+dir=$(readlink -f $1)
+testname=$(basename $dir)
+
+echo "Running test $testname"
+
+eval $(python <<HEREDOC
+import ConfigParser
+f = ConfigParser.ConfigParser()
+f.read('tests.ini')
+for opt in f.options('$testname'):
+  print '%s=%s' % (opt, repr(f.get('$testname', opt)))
+HEREDOC
+)
+
+# Anything that fails after this point is a problem.
+set -e
+
+# Environment setup
+echo "Setting up the environment:"
+. ../setup-env.sh $dir $dir
+
+# Clean up any failing runs and then build everything
+echo -e "\n\nBuilding:"
+make -C $dir clean
+make -C $dir
+
+echo -e "\n\nIndexing:"
+../dxr-index.py -f $dxrconfig -c xref
+
+echo -e "\n\nChecking:"
+python $verifier $testname
