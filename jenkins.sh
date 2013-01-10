b3071eb7038908e2a2ddb7a82515a809f4d81a07
Erik Rose <erik@mozilla.com>
2013-01-09 16:59:26 -0800

    State what jenkins.sh does.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
+# Kicks off the Jenkins tests.
+
 /opt/vagrant/bin/vagrant up
 /opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make test'
 # Should we explicitly destroy the vagrant box here?
