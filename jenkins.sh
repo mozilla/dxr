51dce18897eb44ae16a0418ff080d6f80f0b336e
Erik Rose <erik@mozilla.com>
2013-01-11 13:53:11 -0800

    Destroy the vagrant box at the end of each Jenkins run.
    This is what the vagrant Jenkins plugin does. I suspect state is leaking from run to run without this.

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,4 @@
 
 /opt/vagrant/bin/vagrant up
 /opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make test'
-# Should we explicitly destroy the vagrant box here?
+/opt/vagrant/bin/vagrant destroy
