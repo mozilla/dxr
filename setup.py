1ac180bf53bdcbedbcee087367eb05c23970555d
Erik Rose <erik@mozilla.com>
2013-01-14 15:26:43 -0800

    Move some files around in preparation for turning the Python parts into a legitimate package. (Tests should fail.)
    The dxr folder is going to form the core of an installable package. search.py, query.py, and utils.py never change; they just get copied on build. Thus, those can live in the installed package, and the build can just be a config file and .htaccess. To serve, run dxr-serve.py --config=/my/build/dir. We can explain that in the last 2 lines output from dxr-build, much as we currently do with test-server. For WSGI, we can pass in the path to the config file in an env var.
    
    Because this reduces repetition, it makes dev easier: we can just edit the single, installed (with python setup.py develop) version of the server, and git will pick it up—no more having to edit an untracked build artifact. More standard installation and project layout probably won't hurt our community adoption, either.
    
    Note that the package won't encompass the entire project, as there's no nice way to make setup.py build trilite and the clang plugins (that I know of).

---
+++ setup.py
@@ -0,0 +1,37 @@
+import sys
+
+# Prevent spurious errors during `python setup.py test`, a la
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dxr',
+    version='0.1',
+    description='Source code static analysis and browsing',
+    long_description=open('README.mkd').read(),
+    author='Erik Rose',
+    author_email='erik@mozilla.com',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup']),
+    url='https://github.com/mozilla/dxr',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Software Development'
+        ],
+    keywords=['lxr', 'static analysis', 'source code']
+)
