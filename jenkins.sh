afe09bf3eb74d0e8a371e04710e48939382c233b
James Abbatiello <abbeyj at gmail.com>
2013-01-15 16:46:42 -0500

    Make ls in debugging output recursive

--- jenkins.sh
+++ jenkins.sh
@@ -2,9 +2,9 @@
 # Kicks off the Jenkins tests.
 
 echo "Starting jenkins.sh"
-ls -al
+ls -alR
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && ls -al && make test'
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && ls -alR && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 echo "Ending jenkins.sh"
