251c5dc1951d3613fd34a9cff5b8d28189edf35e
James Abbatiello <abbeyj at gmail.com>
2013-01-15 16:39:44 -0500

    Add debugging output to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # Kicks off the Jenkins tests.
 
+echo "Starting jenkins.sh"
+ls -al
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make test'
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && ls -al && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
+echo "Ending jenkins.sh"
 exit $RESULT
