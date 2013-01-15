657529c4105857983c5c31b894d9c52d84e3c915
James Abbatiello <abbeyj at gmail.com>
2013-01-15 16:52:52 -0500

    Remove debugging output and make jenkins do "make clean" so old outputs can't confuse things.

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # Kicks off the Jenkins tests.
 
-echo "Starting jenkins.sh"
-ls -alR
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && ls -alR && make test'
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make clean && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
-echo "Ending jenkins.sh"
 exit $RESULT
