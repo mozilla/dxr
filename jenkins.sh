07afcace70a557aa2bc9abe9a530d53cf5272a72
James Abbatiello <abbeyj at gmail.com>
2013-01-15 16:39:44 -0500

    Make jenkins do "make clean" so old outputs can't confuse things.

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 # Kicks off the Jenkins tests.
 
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make test'
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make clean && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 exit $RESULT
