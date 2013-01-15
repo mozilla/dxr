7814c860b830fcbbbec4a2b222299ee2923feb94
Erik Rose <erik@mozilla.com>
2013-01-15 14:12:26 -0800

    Load the instance-local config into Flask's config system.
    Capitalize the config keys so we don't have to override any Flask stuff; Flask requires capitals.

--- server/config.py
+++ server/config.py
@@ -1,10 +1,10 @@
 # Settings, will be filled out by ./dxr-build -f FILE --server
 # See create_server() in dxr-build.py
 
-trees                 = ${trees}
-wwwroot               = ${wwwroot}
-template_parameters   = ${template_parameters}
-generated_date        = ${generated_date}
+TREES                 = ${trees}
+WWW_ROOT              = ${wwwroot}
+TEMPLATE_PARAMETERS   = ${template_parameters}
+GENERATED_DATE        = ${generated_date}
 
 # The right hand side of assignments above will be filled using the string.Template
 # function builtin to python. Using repr, we save simple variables this way for
