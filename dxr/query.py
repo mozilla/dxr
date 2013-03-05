2043cf5ae187a9cf3bde77b41d7556645b297604
James Abbatiello <abbeyj at gmail.com>
2013-03-05 17:56:24 -0500

    Make merge_extents and fix_extents_overlap be methods of Query

--- dxr/query.py
+++ dxr/query.py
@@ -171,12 +171,12 @@ class Query:
                 for i in xrange(0, len(extents), 8):
                     s, e = struct.unpack("II", extents[i:i+8])
                     matchExtents.append((s, e, []))
-                elist.append(fix_extents_overlap(sorted(matchExtents)))
+                elist.append(self.fix_extents_overlap(sorted(matchExtents)))
 
             for f in filters:
                 for e in f.extents(self.conn, self, fileid):
                     elist.append(e)
-            offsets = list(merge_extents(*elist))
+            offsets = list(self.merge_extents(*elist))
             if flask.g._should_explain:
                 continue
 
@@ -342,6 +342,55 @@ class Query:
         # Okay we've got nothing
         return None
 
+    def merge_extents(self, *elist):
+        """
+            Take a list of extents generators and merge them into one stream of extents
+            overlapping extents will be split in two, this means that they will start
+            and stop at same location.
+            Here we assume that each extent is a triple as follows:
+                (start, end, keyset)
+
+            Where keyset is a list of something that should be applied to the extent
+            between start and end.
+        """
+        elist = [genWrap(e) for e in elist]
+        elist = [e for e in elist if e.next()]
+        while len(elist) > 0:
+            start = min((e.value[0] for e in elist))
+            end = min((e.value[1] for e in elist if e.value[0] == start))
+            keylist = []
+            for e in (e for e in elist if e.value[0] == start):
+                for k in e.value[2]:
+                    if k not in keylist:
+                        keylist.append(k)
+                e.value = (end, e.value[1], e.value[2])
+            yield start, end, keylist
+            elist = [e for e in elist if e.value[0] < e.value[1] or e.next()]
+
+    def fix_extents_overlap(self, extents):
+        """
+            Take a sorted list of extents and yield the extents without overlapings.
+            Assumes extents are of similar format as in merge_extents
+        """
+        # There must be two extents for there to be an overlap
+        while len(extents) >= 2:
+            # Take the two next extents
+            start1, end1, keys1 = extents[0]
+            start2, end2, keys2 = extents[1]
+            # Check for overlap
+            if end1 <= start2:
+                # If no overlap, yield first extent
+                yield start1, end1, keys1
+                extents = extents[1:]
+                continue
+            # If overlap, yield extent from start1 to start2
+            if start1 != start2:
+                yield start1, start2, keys1
+            extents[0] = (start2, end1, keys1 + keys2)
+            extents[1] = (end1, end2, keys2)
+        if len(extents) > 0:
+            yield extents[0]
+
 
 def like_escape(val):
     """ Escape for usage in as argument to the LIKE operator """
@@ -364,55 +413,6 @@ class genWrap:
         except StopIteration:
             self.value = None
             return False
-
-def merge_extents(*elist):
-    """
-        Take a list of extents generators and merge them into one stream of extents
-        overlapping extents will be split in two, this means that they will start
-        and stop at same location.
-        Here we assume that each extent is a triple as follows:
-            (start, end, keyset)
-
-        Where keyset is a list of something that should be applied to the extent
-        between start and end.
-    """
-    elist = [genWrap(e) for e in elist]
-    elist = [e for e in elist if e.next()]
-    while len(elist) > 0:
-        start = min((e.value[0] for e in elist))
-        end = min((e.value[1] for e in elist if e.value[0] == start))
-        keylist = []
-        for e in (e for e in elist if e.value[0] == start):
-            for k in e.value[2]:
-                if k not in keylist:
-                    keylist.append(k)
-            e.value = (end, e.value[1], e.value[2])
-        yield start, end, keylist
-        elist = [e for e in elist if e.value[0] < e.value[1] or e.next()]
-
-def fix_extents_overlap(extents):
-    """
-        Take a sorted list of extents and yield the extents without overlapings.
-        Assumes extents are of similar format as in merge_extents
-    """
-    # There must be two extents for there to be an overlap
-    while len(extents) >= 2:
-        # Take the two next extents
-        start1, end1, keys1 = extents[0]
-        start2, end2, keys2 = extents[1]
-        # Check for overlap
-        if end1 <= start2:
-            # If no overlap, yield first extent
-            yield start1, end1, keys1
-            extents = extents[1:]
-            continue
-        # If overlap, yield extent from start1 to start2
-        if start1 != start2:
-            yield start1, start2, keys1
-        extents[0] = (start2, end1, keys1 + keys2)
-        extents[1] = (end1, end2, keys2)
-    if len(extents) > 0:
-        yield extents[0]
 
 
 class SearchFilter:
