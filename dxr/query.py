036b1e5971bd4be48c803df6157a85c136834419
James Abbatiello <abbeyj at gmail.com>
2013-03-05 15:47:54 -0500

    Use flask.g instead of flask.request to hold private per-request state.

--- dxr/query.py
+++ dxr/query.py
@@ -100,19 +100,19 @@ class Query:
 #TODO Use named place holders in filters, this would make the filters easier to write
 
 def _execute_sql(conn, sql, *parameters):
-    if flask.request._explain:
-        flask.request._sql_profile.append({
+    if flask.g._explain:
+        flask.g._sql_profile.append({
             "sql" : sql,
             "parameters" : parameters[0] if len(parameters) >= 1 else [],
             "explanation" : conn.execute("EXPLAIN QUERY PLAN " + sql, *parameters)
         })
         start_time = time.time()
     res = conn.execute(sql, *parameters)
-    if flask.request._explain:
+    if flask.g._explain:
         # fetch results eagerly so we can get an accurate time for the entire operation
         res = res.fetchall()
-        flask.request._sql_profile[-1]["elapsed_time"] = time.time() - start_time
-        flask.request._sql_profile[-1]["nrows"] = len(res)
+        flask.g._sql_profile[-1]["elapsed_time"] = time.time() - start_time
+        flask.g._sql_profile[-1]["nrows"] = len(res)
     return res
 
 # Fetch results using a query,
@@ -121,8 +121,8 @@ def fetch_results(conn, query,
                                     offset = 0, limit = 100,
                                     explain = False,
                                     markup = "<b>", markdown = "</b>"):
-    flask.request._explain = explain
-    flask.request._sql_profile = []
+    flask.g._explain = explain
+    flask.g._sql_profile = []
     sql = """
         SELECT files.path, files.icon, trg_index.text, files.id,
         extents(trg_index.contents)
@@ -171,7 +171,7 @@ def fetch_results(conn, query,
             for e in f.extents(conn, query, fileid):
                 elist.append(e)
         offsets = list(merge_extents(*elist))
-        if flask.request._explain:
+        if flask.g._explain:
             continue
 
         lines = []
@@ -237,8 +237,8 @@ def fetch_results(conn, query,
                 ret.append((i, arr[i]))
         return ret
 
-    for i in range(len(flask.request._sql_profile)):
-        profile = flask.request._sql_profile[i]
+    for i in range(len(flask.g._sql_profile)):
+        profile = flask.g._sql_profile[i]
         yield ("",
                       "sql %d (%d row(s); %s seconds)" % (i, profile["nrows"], profile["elapsed_time"]),
                       number_lines(profile["sql"].split("\n")))
