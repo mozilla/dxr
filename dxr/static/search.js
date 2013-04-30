9aca661b396ae0f2920dba55499377118768e1f6
James Abbatiello <abbeyj at gmail.com>
2013-04-30 15:19:46 -0400

    Notification when there is a search in progress. Now with less flicker.

--- dxr/static/search.js
+++ dxr/static/search.js
@@ -151,6 +151,18 @@ function setFetchResultsTimer(){
   _fetch_results_timer = setTimeout(fetch_results, timeout);
 }
 
+var _in_progress_timer = null;
+function clearInProgressTimer() {
+  if (_in_progress_timer) clearTimeout(_in_progress_timer);
+  _in_progress_timer = null;
+}
+function setInProgressTimer(){
+  var timeout = 300;  // use 300 ms
+  clearInProgressTimer();
+  _in_progress_timer = setTimeout(function() {
+    dxr.setTip("Search in progress ...");
+  }, timeout);
+}
 
 
 // Current request
@@ -196,6 +208,7 @@ function fetch_results(display_fetcher){
         state.offset += data["results"].length;
       }
       // Display a nice tip
+      clearInProgressTimer();
       dxr.setTip("Incremental search results in " + data["time"].toFixed(3) + "s");
       var content = document.getElementById("content");
       // Clear results if necessary
@@ -245,7 +258,7 @@ function fetch_results(display_fetcher){
 
   // Start a new request
   request.open("GET", createSearchUrl(dxr.tree(), params), true);
-  dxr.setTip("Search in progress ...");
+  setInProgressTimer();
   request.send();
 }
 
