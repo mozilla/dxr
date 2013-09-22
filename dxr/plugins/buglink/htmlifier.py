c3372bf30936133b3696667ac34931b4b4ceb501
Nicholas Cameron <ncameron@mozilla.com>
2013-09-22 15:37:10 +1200

    Add plugin_buglink_url as an alternative to plugin_buglink_bugzilla for buglink plugin

--- dxr/plugins/buglink/htmlifier.py
+++ dxr/plugins/buglink/htmlifier.py
@@ -3,24 +3,28 @@ import re
 import sys
 
 # Global variables
-bugzilla  = None
+url       = None
 name      = None
 bugFinder = None
 
 # Load global variables
 def load(tree, conn):
-    global bugzilla
-    # Get bugzilla link
-    if hasattr(tree, 'plugin_buglink_bugzilla'):
-        bugzilla = tree.plugin_buglink_bugzilla
-    else:
-        print >> sys.stderr, "buglink plugin needs plugin_buglink_bugzilla configuration key"
-        sys.exit(1)
+    global url
+    global name
+    global bugFinder
     # Get bug tracker name
     if hasattr(tree, 'plugin_buglink_name'):
         name = tree.plugin_buglink_name
     else:
         print >> sys.stderr, "buglink plugin needs plugin_buglink_name configuration key"
+        sys.exit(1)
+    # Get link
+    if hasattr(tree, 'plugin_buglink_bugzilla'):
+        url = tree.plugin_buglink_bugzilla
+    elif hasattr(tree, 'plugin_buglink_url'):
+        url = tree.plugin_buglink_url
+    else:
+        print >> sys.stderr, "buglink plugin needs plugin_buglink_bugzilla or plugin_buglink_url configuration key"
         sys.exit(1)
     # Get bug finder regex
     if hasattr(tree, 'plugin_buglink_regex'):
@@ -39,7 +43,7 @@ class BugLinkHtmlifier(object):
             yield m.start(0), m.end(0), [{
                 'text':     "Lookup #%s" % bug,
                 'title':    "Find this bug number at %s" % name,
-                'href':     bugzilla % bug,
+                'href':     url % bug,
                 'icon':     'buglink'
             }]
 
