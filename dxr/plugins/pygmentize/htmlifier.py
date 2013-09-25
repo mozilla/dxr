2492ba18f4e1c22148862a64a4578b7d1ba63526
Erik Rose <erik@mozilla.com>
2013-09-25 15:50:10 -0400

    Standardize the style of the pygmentize plugin.

--- dxr/plugins/pygmentize/htmlifier.py
+++ dxr/plugins/pygmentize/htmlifier.py
@@ -1,9 +1,12 @@
-import dxr.plugins
+from os.path import basename
+from fnmatch import fnmatchcase
+
 import pygments
 import pygments.lexers
 from pygments.token import Token
-import os, sys
-import fnmatch
+
+import dxr.plugins
+
 
 token_classes = {
     Token.Keyword: 'k',
@@ -31,26 +34,33 @@ token_classes = {
     Token.Comment.Special: 'c',
     Token.Comment.Preproc: 'p'}
 
+
 class Pygmentizer(object):
-    """ Pygmentizer add syntax regions for file """
+    """Pygmentizer to apply CSS classes to syntax-highlit regions"""
+
     def __init__(self, text, lexer):
-        self.text   = text
-        self.lexer  = lexer
+        self.text = text
+        self.lexer = lexer
+
     def refs(self):
         return []
+
     def regions(self):
         for index, token, text in self.lexer.get_tokens_unprocessed(self.text):
             cls = token_classes.get(token)
             if cls:
                 yield index, index + len(text), cls
+
     def annotations(self):
         return []
+
     def links(self):
         return []
 
 
 def load(tree, conn):
     pass
+
 
 def htmlify(path, text):
     # TODO Enable C++ highlighting using pygments, pending fix for infinite
@@ -59,16 +69,17 @@ def htmlify(path, text):
     if any((path.endswith(e) for e in ('.c', '.cc', '.cpp', '.cxx', '.h', '.hpp'))):
         return None
     # Options and filename
-    options   = {'encoding': 'utf-8'}
-    filename  = os.path.basename(path)
+    options = {'encoding': 'utf-8'}
+    filename = basename(path)
     try:
         lexer = pygments.lexers.get_lexer_for_filename(filename, **options)
     except pygments.util.ClassNotFound:
         # Small hack for js highlighting of jsm files
-        if fnmatch.fnmatchcase(filename, "*.jsm"):
+        if fnmatchcase(filename, "*.jsm"):
             lexer = pygments.lexers.JavascriptLexer(**options)
         else:
             return None
     return Pygmentizer(text, lexer)
+
 
 __all__ = dxr.plugins.htmlifier_exports()
