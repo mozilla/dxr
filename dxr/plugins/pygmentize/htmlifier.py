bbf292aaaa3ed61514b9c299d330872f2b2472c9
Erik Rose <erik@mozilla.com>
2013-10-15 14:17:13 -0400

    Switch from a sequence of linear list lookups to a single hash lookup for a theoretical speed boost.

--- dxr/plugins/pygmentize/htmlifier.py
+++ dxr/plugins/pygmentize/htmlifier.py
@@ -9,29 +9,30 @@ from pygments.token import Token
 import dxr.plugins
 
 
-keyword_tokens = [Token.Keyword,
-                  Token.Keyword.Constant,
-                  Token.Keyword.Declaration,
-                  Token.Keyword.Namespace,
-                  Token.Keyword.Pseudo,
-                  Token.Keyword.Reserved,
-                  Token.Keyword.Type]
-string_tokens = [Token.String,
-                 Token.String.Backtick,
-                 Token.String.Char,
-                 Token.String.Doc,
-                 Token.String.Double,
-                 Token.String.Escape,
-                 Token.String.Heredoc,
-                 Token.String.Interpol,
-                 Token.String.Other,
-                 Token.String.Regex,
-                 Token.String.Single,
-                 Token.String.Symbol]
-comment_tokens = [Token.Comment,
-                  Token.Comment.Multiline,
-                  Token.Comment.Single,
-                  Token.Comment.Special]
+token_classes = {Token.Comment.Preproc: 'p'}
+token_classes.update((t, 'k') for t in [Token.Keyword,
+                                        Token.Keyword.Constant,
+                                        Token.Keyword.Declaration,
+                                        Token.Keyword.Namespace,
+                                        Token.Keyword.Pseudo,
+                                        Token.Keyword.Reserved,
+                                        Token.Keyword.Type])
+token_classes.update((t, 'str') for t in [Token.String,
+                                          Token.String.Backtick,
+                                          Token.String.Char,
+                                          Token.String.Doc,
+                                          Token.String.Double,
+                                          Token.String.Escape,
+                                          Token.String.Heredoc,
+                                          Token.String.Interpol,
+                                          Token.String.Other,
+                                          Token.String.Regex,
+                                          Token.String.Single,
+                                          Token.String.Symbol])
+token_classes.update((t, 'c') for t in [Token.Comment,
+                                        Token.Comment.Multiline,
+                                        Token.Comment.Single,
+                                        Token.Comment.Special])
 
 
 class Pygmentizer(object):
@@ -46,12 +47,9 @@ class Pygmentizer(object):
 
     def regions(self):
         for index, token, text in self.lexer.get_tokens_unprocessed(self.text):
-            cls = None
-            if token in keyword_tokens:        cls = 'k'
-            if token in string_tokens:         cls = 'str'
-            if token in comment_tokens:        cls = 'c'
-            if token is Token.Comment.Preproc: cls = 'p'
-            if cls:   yield index, index + len(text), cls
+            cls = token_classes.get(token)
+            if cls:
+                yield index, index + len(text), cls
 
     def annotations(self):
         return []
