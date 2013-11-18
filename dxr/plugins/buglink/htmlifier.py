3ebcfab19cb148ae5dac543dfe06e36366b91e32
Erik Rose <erik@mozilla.com>
2013-11-18 18:39:27 -0500

    Fix style in buglink htmlifier.
    Declare all globals explicitly. PEP 8 stuff.

--- dxr/plugins/buglink/htmlifier.py
+++ dxr/plugins/buglink/htmlifier.py
@@ -1,23 +1,26 @@
-import dxr.plugins
 import re
 import sys
+
+import dxr.plugins
+
 
 # Global variables
 url       = None
 name      = None
-bugFinder = None
+bug_finder = None
+
 
 # Load global variables
 def load(tree, conn):
-    global url
-    global name
-    global bugFinder
+    global url, name, bug_finder
+
     # Get bug tracker name
     if hasattr(tree, 'plugin_buglink_name'):
         name = tree.plugin_buglink_name
     else:
         print >> sys.stderr, "buglink plugin needs plugin_buglink_name configuration key"
         sys.exit(1)
+
     # Get link
     if hasattr(tree, 'plugin_buglink_bugzilla'):
         url = tree.plugin_buglink_bugzilla
@@ -26,25 +29,28 @@ def load(tree, conn):
     else:
         print >> sys.stderr, "buglink plugin needs plugin_buglink_bugzilla or plugin_buglink_url configuration key"
         sys.exit(1)
+
     # Get bug finder regex
     if hasattr(tree, 'plugin_buglink_regex'):
-        bugFinder = re.compile(tree.plugin_buglink_regex)
+        bug_finder = re.compile(tree.plugin_buglink_regex)
     else:
         # default bug finder regex for backwards compatability
-        bugFinder = re.compile("(?i)bug\s+#?([0-9]+)")  # also used in hg plugin
+        bug_finder = re.compile("(?i)bug\s+#?([0-9]+)")  # also used in hg plugin
+
 
 class BugLinkHtmlifier(object):
     def __init__(self, text):
         self.text = text
 
     def refs(self):
-        for m in bugFinder.finditer(self.text):
+        global name
+        for m in bug_finder.finditer(self.text):
             bug = m.group(1)
             yield m.start(0), m.end(0), ([{
-                'text':     "Lookup #%s" % bug,
-                'title':    "Find this bug number at %s" % name,
-                'href':     url % bug,
-                'icon':     'buglink'
+                'text': "Lookup #%s" % bug,
+                'title': "Find this bug number at %s" % name,
+                'href': url % bug,
+                'icon': 'buglink'
             }], '')
 
     def regions(self):
@@ -56,7 +62,9 @@ class BugLinkHtmlifier(object):
     def links(self):
         return []
 
+
 def htmlify(path, text):
     return BugLinkHtmlifier(text)
+
 
 __all__ = dxr.plugins.htmlifier_exports()
