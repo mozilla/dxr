149b8268ab4197f469e74eb9985bc7cd70630e63
ossreleasefeed <sneethling@mozilla.com>
2013-12-03 14:58:35 +0200

    Fix Bug 945679, disable pointer events on scroll and enable when scroll stops

--- dxr/static/js/dxr.js
+++ dxr/static/js/dxr.js
@@ -4,6 +4,26 @@
 $(function() {
     'use strict';
 
+    /**
+     * Disable and enable event on scroll begin and scroll end.
+     * @see http://www.thecssninja.com/javascript/pointer-events-60fps
+     */
+    var root = document.documentElement;
+    var timer;
+
+    window.addEventListener('scroll', function() {
+        // User scrolling so stop the timeout
+        clearTimeout(timer);
+        // Pointer events has not already been disabled.
+        if (!root.style.pointerEvents) {
+            root.style.pointerEvents = 'none';
+        }
+
+        timer = setTimeout(function() {
+            root.style.pointerEvents = '';
+        }, 500);
+    }, false);
+
     var constants = $('#data'),
         wwwroot = constants.data('root'),
         icons = wwwroot + '/static/icons/',
