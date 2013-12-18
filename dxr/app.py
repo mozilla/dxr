c77e9d7716840cced30fd4b2488a057f14594cf8
Erik Rose <erik@mozilla.com>
2013-12-18 18:19:07 -0500

    Remove the /advanced/ controller, since the advanced search page has been designed away. Fixes bug 951239.

--- dxr/app.py
+++ dxr/app.py
@@ -45,23 +45,6 @@ def index():
     return redirect('%s/%s/source/' % (wwwroot, tree))
 
 
-@dxr_blueprint.route('/advanced')
-def advanced():
-    """Search by all available filters."""
-    config = current_app.config
-
-    # Arguments for the template:
-    arguments = {
-        # Common template variables
-        'wwwroot': config['WWW_ROOT'],
-        'tree': config['TREES'][0],
-        'trees': config['TREES'],
-        'config': config['TEMPLATE_PARAMETERS'],
-        'generated_date': config['GENERATED_DATE']}
-
-    return render_template('advanced.html', **arguments)
-
-
 @dxr_blueprint.route('/<tree>/search')
 def search(tree):
     """Search by regex, caller, superclass, or whatever."""
