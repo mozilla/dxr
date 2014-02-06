fd8683f07c50a8f9561409842f07c0113e7e4a87
Erik Rose <erik@mozilla.com>
2014-02-06 11:35:55 -0500

    "vagrant ssh" doesn't take -t -t. Oops.
    Let's just put an echo in there and flail around.

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,8 @@ git submodule sync
 git submodule update --init --recursive
 
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -t -t -c 'cd /home/vagrant/dxr && make clean && make test'
+echo "DONE PROVISIONING"
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make clean && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 exit $RESULT
