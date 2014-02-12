dac2a26b280448aa22f3574a9ed0d038ee6fee36
balberski <alberskib@gmail.com>
2014-02-12 21:44:26 +0100

    [#961840] Remember state of case checkbox - Fix for url caseSenitivity param

--- dxr/static/js/dxr.js
+++ dxr/static/js/dxr.js
@@ -436,6 +436,17 @@ $(function() {
     // Update the search when the case-sensitive box is toggled, canceling any pending query:
     caseSensitiveBox.on('change', updateLocalStorageAndQueryNow);
 
+
+    var urlCaseSensitive = /case=(\w+)/.exec(location.search)
+    if (urlCaseSensitive){
+        // persisting url case sensitivity param
+        localStorage.setItem('caseSensitive','true' === urlCaseSensitive[1]);
+    }else{
+        // restoring checkbox state from localStorage
+        caseSensitiveBox.prop('checked', 'true' === localStorage.getItem('caseSensitive'));
+    }
+
+
     /**
      * Adds aleading 0 to numbers less than 10 and greater that 0
      *
@@ -477,8 +488,6 @@ $(function() {
         setTimeout(function() {
             window.onpopstate = popStateHandler;
         }, 0);
-        // initialization of case checkbox by value remembered in localStorage
-        $('#case').prop('checked', 'true' === localStorage.getItem('caseSensitive'));
     };
 
     // Reload the page when we go back or forward.
