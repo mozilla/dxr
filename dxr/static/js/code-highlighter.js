dd3fb18c4a6db394d723a7844c07212edf5e0273
Jamon Camisso <jamon@jamon.ca>
2014-03-07 14:37:22 -0500

    refs bug 976213: Refactored and tidied code-highlighter.js & related css.  * Added highlighting on $(document).ready event window.location.hash exists.  * Refactored highlighted element detection to use a pure js method.  * Added generator for window.location.hash corresponding to selected lines.

--- dxr/static/js/code-highlighter.js
+++ dxr/static/js/code-highlighter.js
@@ -2,125 +2,100 @@
 /* globals nunjucks: true, $ */
 
 /**
- * Toggle the .highlighted class on a <code> element with aria-labelledby="num" attribute
- * where num is extracted from the <td id="line-numbers"><span id="num"> structure.
- * This mapping allows clicking on the line numbers spans without interfering with
- * the dynamically overlaid context-menu on each line.
- *
- * One major failing is that holding shift, selecting a group of lines, and then
- * and then selecting another line leads to a complete mess. Not sure what to do there.
+ * This file consists of three major pieces of functionality for a file view:
+ * 1) Multi-select highlight lines with shift key and set window.location.hash
+ * 2) Toggle the .highlighted class for a single line and set window.location.hash
+ * 3) Highlight lines when page loads, if a numbered window.location.hash exists
  */
 
 $(function () {
     'use strict';
     var container = $("#line-numbers");
 
-    //bind to all span.line-number click events
+    //first bind to all .line-number click events only
     container.on("click", ".line-number", function (event) {
-        var order = null,
-            line = null,
-            last_selected_num = null,
-            //get the line number
-            selected_num = $(this).attr('id'),
-            selected = null,
-            last_selected = $(".last-selected");
+        var line = null,
+            lastSelectedNum = null,
+            //get the clicked line number
+            clickedNum = parseInt($(this).attr('id'), 10),
+            selected = null;
 
-        //multiselect on shiftkey press and click
+        //multiselect on shiftkey modifier combined with click
         if (event.shiftKey) {
+            var lastSelected = $(".last-selected");
             // on shift, find last-selected code element
-            // if last_selected_num less than selected_num go back
-            // else if last_selected_num greater than line id, go forward
-            line = $("#" + selected_num);
-            last_selected_num = last_selected.attr('id');
-
-            if (last_selected_num === selected_num) {
+            // if lastSelectedNum less than clickedNum go back
+            // else if lastSelectedNum greater than line id, go forward
+            line = $("#" + clickedNum);
+            lastSelectedNum = parseInt(lastSelected.attr('id'), 10);
+            if (lastSelectedNum === clickedNum) {
                 //toggle a single shiftclicked line
                 line.removeClass("last-selected highlighted clicked");
-
-            } else if (last_selected_num < selected_num) {
+            } else if (lastSelectedNum < clickedNum) {
                 //shiftclick descending down the page
                 line.addClass("clicked");
                 selected = $(".last-selected").nextUntil($(".clicked"));
                 // on last element add last-selected class
                 $(".line-number").removeClass("clicked highlighted");
-
-            } else if (last_selected_num > selected_num) {
+            } else if (lastSelectedNum > clickedNum) {
                 //shiftclick ascending up the page
                 $(".line-number").removeClass("highlighted clicked");
                 line.addClass("clicked");
-                selected = $(".clicked").nextUntil(last_selected);
+                selected = $(".clicked").nextUntil(lastSelected);
             }
-
-            selected.each(function () {
-                selected.addClass("highlighted");
-                var selected_code = $(this).html();
-                if (selected_code === "") {
-                    $(this).html("<br>");
-                }
-            });
+            if (selected.length > 0) {
+                selected.each(function () {
+                    selected.addClass("highlighted");
+                });
+            }
 
             // since all highlighed items are stripped, add one back
-            last_selected.addClass("highlighted");
+            lastSelected.addClass("highlighted");
             line.addClass("highlighted");
+            setWindowHash(clickedNum, lastSelectedNum);
 
-            var last_clicked_line = $(".clicked").attr('id');
-            var last_selected_line = $(".last-selected").attr('id');
-            console.log(last_clicked_line);
-            console.log(last_selected_line);
-            if (last_clicked_line) {
-                window.location.hash = last_clicked_line + "-" + last_selected_line;
-            } else {
-                var last_highlighted_lines = $(".highlighted");
-		var len_last_highlighted_lines = last_highlighted_lines.length - 1;
-                var last_highlighted_line = last_highlighted_lines[len_last_highlighted_lines].id;
-                window.location.hash = last_selected_line + "-" + last_highlighted_line;
-            }
+        //single non-shift modified click toggle here
         } else {
-            //single non-shift click toggle
-            line = $("#" + selected_num);
-            if (last_selected.attr('id') !== selected_num) {
+            var lastSelected = $(".last-selected");
+            line = $("#" + clickedNum);
+
+            if (parseInt(lastSelected.attr('id'), 10) !== clickedNum) {
                 $(".highlighted").removeClass("last-selected highlighted");
                 line.toggleClass("last-selected highlighted");
-
+                window.location.hash = line.attr('id');
             } else {
                 $(".highlighted").removeClass("last-selected highlighted");
+                window.location.hash = '';
             }
-
-            if (line.text() === "") {
-                line.html("<br>");
-            }
-            window.location.hash = line.attr('id');
         }
-
     });
 
+    //set the window.location.hash to the highlighted lines
+    function setWindowHash(clickedNum, lastSelectedNum) {
+        var windowHighlightedLines = null;
+        //order of line numbers matters in the url, detect it here
+        if (clickedNum < lastSelectedNum) {
+            windowHighlightedLines = clickedNum + "-" + lastSelectedNum;
+        } else {
+            windowHighlightedLines = lastSelectedNum + "-" + clickedNum;
+        }
+        window.location.hash = windowHighlightedLines;
+    }
+
+    //highlight line(s) if someone visits a url directly with an #anchor
     $(document).ready(function () {
-        var hash = window.location.hash.replace("#", "");
-        var lines = hash.split("-");
+        var hash = window.location.hash.replace("#", ""),
+            lines = hash.split("-");
 
         //handle multi-line highlights
         if (lines.length > 1) {
-            var start_line = lines[0];
-            var end_line = lines[1];
-            var selected = $("#" + start_line).nextUntil("#" + end_line);
-            $("#" + start_line).addClass('highlighted');
-            $("#" + end_line).addClass('highlighted');
-
-            selected.each(function () {
-                $(this).addClass('highlighted');
-                var selected_code = $(this).html();
-                if (selected_code === "") {
-                    $(this).html("<br>");
-                }
-            });
-
-        } else {
-            //handle a single line highlight, 'lines' is one line number here
-            var line = $("#" + lines);
-            line.addClass('highlighted');
-            if (line.text() === "") {
-                line.html("<br>");
+            for (var i = lines[0]; i <= lines[1]; ++i) {
+                var line = document.getElementById(i);
+                line.classList.add('highlighted');
             }
+        //handle a single line highlight, 'lines' is one line number here
+        } else {        
+            document.getElementById(lines[0]).classList.add('highlighted');
         }
     });
 
