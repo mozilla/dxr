a22d5e2b38f6b5bc85dfe00ce215c53d7bfabefc
James Abbatiello <abbeyj at gmail.com>
2014-03-25 17:26:04 -0400

    Try to fix tests under clang 3.3.

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -1,5 +1,7 @@
 """Tests for searches about warnings"""
 
+import commands
+import re
 from dxr.testing import SingleFileTestCase, MINIMAL_MAIN
 
 
@@ -34,14 +36,25 @@ class MultipleOnSameLineWarningTests(SingleFileTestCase):
         }
         """ + MINIMAL_MAIN
 
+    def _clang_at_least(self, version):
+        output = commands.getoutput("clang --version")
+        if not output:
+            return False
+        match = re.match("clang version ([0-9]+\.[0-9]+)", output[0])
+        if not match:
+            return False
+        return float(match.group(1)) >= version
+
     def test_warning(self):
-        self.found_line_eq(
-            'warning:"logical not *"', 'if (!x <b>&lt;</b> 3)')
+        if self._clang_at_least(3.4):
+            self.found_line_eq(
+                'warning:"logical not *"', 'if (!x <b>&lt;</b> 3)')
         self.found_line_eq(
             'warning:"* always true"', 'if (<b>!x</b> &lt; 3)')
 
     def test_warning_opt(self):
-        self.found_line_eq(
-            'warning-opt:-Wlogical-not-parentheses', 'if (!x <b>&lt;</b> 3)')
+        if self._clang_at_least(3.4):
+            self.found_line_eq(
+                'warning-opt:-Wlogical-not-parentheses', 'if (!x <b>&lt;</b> 3)')
         self.found_line_eq(
             'warning-opt:-Wtautological-constant-out-of-range-compare', 'if (<b>!x</b> &lt; 3)')
