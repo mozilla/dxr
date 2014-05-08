979d27b13f0edeb825c1085def8f18d54d5e5b9b
Erik Rose <erik@mozilla.com>
2014-05-08 10:49:01 -0400

    Fix invalid query parser tests.
    They have to adapt from the result being a dict of filter types to it being a list of terms.

--- tests/test_query_parser.py
+++ tests/test_query_parser.py
@@ -17,87 +17,104 @@ class VisitorTests(TestCase):
     def test_overall(self):
         """Test the overall structure."""
         eq_(self.visit('regexp:(?i)snork'),
-            {'regexp': [{'arg': '(?i)snork',
-                         'not': False,
-                         'case_sensitive': False,
-                         'qualified': False}]})
+            [{'arg': '(?i)snork',
+              'type': 'regexp',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_tricksy_orphanses(self):
         """Try to trick the parser into prematurely committing to various
         classifications."""
         eq_(self.visit('- -+ +- -+fred +type: +-type:hey type: smoo hi:mom +boo'),
-            {'text': [{'arg': '-',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+',
-                       'not': True,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+-',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+fred',
-                       'not': True,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+type:',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+-type:hey',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': 'type:',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': 'smoo',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': 'hi:mom',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': '+boo',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False}]})
+            [{'arg': '-',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+',
+              'type': 'text',
+              'not': True,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+-',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+fred',
+              'type': 'text',
+              'not': True,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+type:',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+-type:hey',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'type:',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'smoo',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'hi:mom',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': '+boo',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_normal_things(self):
         """Make sure normal, everyday things that should work do."""
         eq_(self.visit('regexp:smoo -regexp:foo|bar -baz qux type:yeah'),
-            {'regexp': [{'arg': 'smoo',
-                         'not': False,
-                         'case_sensitive': False,
-                         'qualified': False},
-                        {'arg': 'foo|bar',
-                         'not': True,
-                         'case_sensitive': False,
-                         'qualified': False}],
-             'text':   [{'arg': 'baz',
-                         'not': True,
-                         'case_sensitive': False,
-                         'qualified': False},
-                        {'arg': 'qux',
-                         'not': False,
-                         'case_sensitive': False,
-                         'qualified': False}],
-             'type':   [{'arg': 'yeah',
-                         'not': False,
-                         'case_sensitive': False,
-                         'qualified': False}]})
+            [{'arg': 'smoo',
+              'type': 'regexp',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'foo|bar',
+              'type': 'regexp',
+              'not': True,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'baz',
+              'type': 'text',
+              'not': True,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'qux',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'yeah',
+              'type': 'type',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_qualified(self):
         """Make sure fully-qualified filters are recognized."""
         eq_(self.visit('+type:Snork'),
-            {'type': [{'arg': 'Snork',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': True}]})
+            [{'arg': 'Snork',
+              'type': 'type',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': True}])
 
     def test_unclosed_quotes(self):
         """An unclosed quoted string should be considered as if it were closed.
@@ -108,10 +125,11 @@ class VisitorTests(TestCase):
 
         """
         eq_(self.visit('"this here thing'),
-            {'text': [{'arg': 'this here thing',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False}]})
+            [{'arg': 'this here thing',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_literal_quotes(self):
         """Make sure we can express literal quotes when we want to.
@@ -120,30 +138,34 @@ class VisitorTests(TestCase):
 
         """
         eq_(self.visit(""" '"this' 'here"' "'thing'" """),
-            {'text': [{'arg': '"this',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': 'here"',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False},
-                      {'arg': "'thing'",
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False}]})
+            [{'arg': '"this',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': 'here"',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False},
+             {'arg': "'thing'",
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_bare_unicode(self):
         """Make sure non-ASCII chars are recognized in bare text."""
         eq_(self.visit(u'börg'),
-            {'text': [{'arg': u'börg',
-                       'not': False,
-                       'case_sensitive': False,
-                       'qualified': False}]})
+            [{'arg': u'börg',
+              'type': 'text',
+              'not': False,
+              'case_sensitive': False,
+              'qualified': False}])
 
     def test_empty(self):
         """An empty query shouldn't give a ParseError."""
-        eq_(self.visit(''), {})
+        eq_(self.visit(''), [])
 
 
 # Not in VisitorTests because nose doesn't support test generators in TestCase
