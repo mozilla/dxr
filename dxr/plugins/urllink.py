f24e234241b594f15f4432466d5a5f72e6abb38a
Erik Rose <erik@mozilla.com>
2014-06-16 11:11:08 -0400

    Make urllink notionally line-based.
    Of course, its attr names don't match with the FileIndexer constructors, so it doesn't work yet.

--- dxr/plugins/urllink.py
+++ dxr/plugins/urllink.py
@@ -12,19 +12,20 @@ url_re = re.compile("\[(https?://[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+\.[A-
 class FileIndexer(dxr.plugins.FileIndexer):
     def line_refs(self):
         if isinstance(self.contents, unicode):
-            for m in url_re.finditer(self.text):
-                if m.group(1):
-                    url = m.group(1)
-                    start, end = m.start(1), m.end(1)
-                elif m.group(2):
-                    url = m.group(2)
-                    start, end = m.start(2), m.end(2)
-                else:
-                    url = m.group(3)
-                    start, end = m.start(3), m.end(3)
-                yield start, end, ([{
-                    'html':   "Follow link",
-                    'title':  "Visit %s" % url,
-                    'href':   url,
-                    'icon':   'external_link'
-                }], '', None)
+            for line in self.contents.splitlines():
+                for m in url_re.finditer(line):
+                    if m.group(1):
+                        url = m.group(1)
+                        start, end = m.start(1), m.end(1)
+                    elif m.group(2):
+                        url = m.group(2)
+                        start, end = m.start(2), m.end(2)
+                    else:
+                        url = m.group(3)
+                        start, end = m.start(3), m.end(3)
+                    yield start, end, ([{
+                        'html':   "Follow link",
+                        'title':  "Visit %s" % url,
+                        'href':   url,
+                        'icon':   'external_link'
+                    }], '', None)
