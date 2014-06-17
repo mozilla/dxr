ddf58205cb4fc281d5f13d76e0a0045d3c4eb39c
Erik Rose <erik@mozilla.com>
2014-06-16 20:12:35 -0400

    Group found URLs by line as the API requires.
    Also simplify URL regex. We were going through an awful lot of trouble to include any surrounding [] or () in a link's href.

--- dxr/plugins/urllink.py
+++ dxr/plugins/urllink.py
@@ -4,28 +4,21 @@ import dxr.plugins
 
 
 # From http://stackoverflow.com/a/1547940
-url_re = re.compile("\[(https?://[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+\.[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+)\]"
-       "|\((https?://[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+\.[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+)\)"
-       "|(https?://[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+\.[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+)")
+url_re = re.compile("https?://[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+\.[A-Za-z0-9\-\._~:\/\?#[\]@!\$&'()*\+,;=%]+")
 
 
 class FileToIndex(dxr.plugins.FileToIndex):
     def refs_by_line(self):
         if isinstance(self.contents, unicode):
             for line in self.contents.splitlines():
-                for m in url_re.finditer(line):
-                    if m.group(1):
-                        url = m.group(1)
-                        start, end = m.start(1), m.end(1)
-                    elif m.group(2):
-                        url = m.group(2)
-                        start, end = m.start(2), m.end(2)
-                    else:
-                        url = m.group(3)
-                        start, end = m.start(3), m.end(3)
-                    yield start, end, ([{
-                        'html':   "Follow link",
-                        'title':  "Visit %s" % url,
-                        'href':   url,
-                        'icon':   'external_link'
-                    }], '', None)
+                yield list(self._links_on_line(line))
+
+    def _links_on_line(self, line):
+        """Return a sorted iterable of the refs in one line of text."""
+        for m in url_re.finditer(line):
+            url = m.group(0)
+            yield m.start(0), m.end(0), ([{
+                'html': 'Follow link',
+                'title': 'Visit %s' % url,
+                'href': url,
+                'icon': 'external_link'}], None)
