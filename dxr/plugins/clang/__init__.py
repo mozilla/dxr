678ebd608a389c32472530992ad3709acd003ef8
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-07-22 22:45:10 -0700

    scaffolding for FileToIndex

--- dxr/plugins/clang/__init__.py
+++ dxr/plugins/clang/__init__.py
@@ -5,88 +5,38 @@ import os
 from functools import wraps
 from funcy import merge, partial, decorator
 
-from dxr.plugins import TreeToIndex
+from dxr.plugins import FileToIndex
+from dxr.plugins.utils import StatefulTreeToIndex
 from dxr.plugins.clang.condense import load_csv, build_inhertitance
+
 
 PLUGIN_NAME = 'clang'
 
-class CXXTreeToIndex(TreeToIndex):
-    """Clang TreeToIndexer instance."""
-    def environment(self, vars_):
-        # Setup environment variables for inspecting clang as runtime
-        # We'll store all the havested metadata in the plugins temporary folder.
-        tree = self.tree
-        self.temp_folder = os.path.join(tree.temp_folder, 'plugins', PLUGIN_NAME)
-        plugin_folder = os.path.join(tree.config.plugin_folder, PLUGIN_NAME)
-        flags = [
-            '-load', os.path.join(plugin_folder, 'libclang-index-plugin.so'),
-            '-add-plugin', 'dxr-index',
-            '-plugin-arg-dxr-index', tree.source_folder
-        ]
-        flags_str = ""
-        for flag in flags:
-            flags_str += ' -Xclang ' + flag
-        env = {
-            'CC': "clang %s" % flags_str,
-            'CXX': "clang++ %s" % flags_str,
-            'DXR_CLANG_FLAGS': flags_str,
-            'DXR_CXX_CLANG_OBJECT_FOLDER': tree.object_folder,
-            'DXR_CXX_CLANG_TEMP_FOLDER': self.temp_folder,
-        }
-        env['DXR_CC'] = env['CC']
-        env['DXR_CXX'] = env['CXX']
-        
-        return merge(vars_, env)
 
-        
-    def post_build(self):
-        condensed = load_csv(self.temp_folder, csv_path=None, only_impl=True)
-        self.inherit = build_inhertitance(condensed)
+class ClangFileToIndex(FileToIndex):
+    def __init__(self, path, contents, tree, inherit):
+        super(ClangFileToIndex, self).__init__(path, contents, tree)
+        self.inherit = inherit
+
+    def needles(self):
+        return []
+
+    def needles_by_line(self):
+        return []
+
+    def refs_by_line(self):
+        return []
+
+    def regions_by_line(self):
+        return []
+
+    def annotations_by_line(self):
+        return []
 
 
-    def file_to_index(self, path, contents):
-        return ClangFileToIndex(path, contents, self.tree, self.inherit)
-
-
-@decorator
-def transition(call, start, end):
-    self = call._args[0]
-    if self.state != start:
-        raise RuntimeError('In state {0}, expected {1}'.format(
-            self.state, start))
-    out = call()
-    self.state = end
-    return out
-
-class StatefulTreeToIndex(TreeToIndex):
+class ClangTreeToIndex(StatefulTreeToIndex):
     def __init__(tree, state_machine):
-        super(StatefulTreeToIndex, self).__init__(tree)
-        self.state_machine = state_machine()
-        next(self.state_machine)
-        self.state = "start"
-        
-
-    @transition('start', 'environment')
-    def environment(self, vars):
-        self.state_machine.send(vars)
-        return next(self.state_machine)
-
-    @transition('environment', 'pre_build')
-    def pre_build(self):
-        next(self.state_machine)
-
-
-    @transition('pre_build', 'post_build')
-    def post_build(self):
-        self.file_indexer = next(self.state_machine)
-
-    @transition('post_build', 'post_build')
-    def file_to_index(self, path, contents):
-        return self.file_indexer(path, contents)
-
-
-class ClangFileToIndex(File_To_Index):
-    pass # ClangFileToIndex definition
+        super(ClangTreeToIndex, self).__init__(tree, clang_indexer)
 
 
 def env(vars_, tree):
@@ -115,20 +65,12 @@ def env(vars_, tree):
     return merge(vars_, env)
 
 
-@tree_to_indexer # Creates TreeIndexer
 def clang_indexer(tree):
     vars_ = yield
-    # Env setup
-    yield env(vars_, tree)
-
+    yield env(vars_, tree) # ENV SETUP
     # PREBUILD
-
     yield # BUILD STEP
-    
-    # POST BUILD
-
-    # Build up only the inheritance part of the CSV
+    # POSTBUILD
     condensed = load_csv(temp_folder, csv_path=None, only_impl=True)
     inherit = build_inhertitance(condensed)
-
-    yield partial(ClangFileToIndex, inherit)
+    yield partial(ClangFileToIndex, inherit=inherit)
--- dxr/plugins/utils.py
+++ dxr/plugins/utils.py
@@ -3,6 +3,8 @@
 from collections import namedtuple
 from itertools import ifilter
 
+from dxr.plugins import TreeToIndex
+
 Extent = namedtuple('Extent', ['start', 'end'])
 # Note that if offset is a Maybe Int, if not present it's None
 Position = namedtuple('Position', ['offset', 'row', 'col'])
@@ -32,3 +34,40 @@ def is_function((_, obj)):
         return False
     type_ = obj['!type']
     return hasattr(type_, 'input') and hasattr(type_, 'output')
+
+
+@decorator
+def transition(call, start, end):
+    self = call._args[0]
+    if self.state != start:
+        raise RuntimeError('In state {0}, expected {1}'.format(
+            self.state, start))
+    out = call()
+    self.state = end
+    return out
+
+
+class StatefulTreeToIndex(TreeToIndex):
+    """Start -> Env -> Prebuild -> Postbuild"""
+    def __init__(tree, state_machine):
+        super(StatefulTreeToIndex, self).__init__(tree)
+        self.state_machine = state_machine()
+        next(self.state_machine)
+        self.state = "start"
+        
+    @transition('start', 'environment')
+    def environment(self, vars):
+        self.state_machine.send(vars)
+        return next(self.state_machine)
+
+    @transition('environment', 'pre_build')
+    def pre_build(self):
+        next(self.state_machine)
+
+    @transition('pre_build', 'post_build')
+    def post_build(self):
+        self.file_indexer = next(self.state_machine)
+
+    @transition('post_build', 'post_build')
+    def file_to_index(self, path, contents):
+        return self.file_indexer(path, contents)
