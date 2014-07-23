0f63b47a9c6c41975430f1aba17d8e1ca2635f50
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-07-23 11:04:17 -0700

    incorrect order test fixed

--- tests/test_clang_plugin.py
+++ tests/test_clang_plugin.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 from itertools import permutations, ifilter
+from operator import itemgetter
+from functools import partial
 
 from mock import MagicMock
 from nose import SkipTest
@@ -7,16 +9,20 @@ from nose.tools import eq_
 
 from dxr.plugins.clang import ClangTreeToIndex
 
+
 def smoke_test():
     c = ClangTreeToIndex('test')
 
+
 CORRECT_ORDER = [('environment', [{}]), ('pre_build', []), ('post_build', []),
                  ('file_to_index', ['foo', 'bar'])]
+
 
 def check_order(order):
     c = ClangTreeToIndex(MagicMock())
     for cmd, args in order:
         getattr(c, cmd)(*args)
+
 
 def check_incorrect_order(order):
     try:
@@ -25,11 +31,14 @@ def check_incorrect_order(order):
     except RuntimeError:
         pass
 
+
 def correct_order_test():
     check_order(CORRECT_ORDER)
+
     
 def incorrect_order_tests():
+    only_cmds = partial(map, itemgetter(0))
     for order in permutations(CORRECT_ORDER):
-        if order == CORRECT_ORDER:
+        if only_cmds(order) == only_cmds(CORRECT_ORDER):
             continue
         yield check_incorrect_order, order
