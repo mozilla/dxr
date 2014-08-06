684e18e66d20397a6300c8aa24e315d9c116c4fb
vedadux <vedadux@gmail.com>
2014-08-06 21:16:58 +0200

    Commiting my changes to the fork.

--- dxr/static/js/dxr.js
+++ dxr/static/js/dxr.js
@@ -126,7 +126,43 @@ $(function() {
 
         return pathLines;
     }
+    
+    /**
+     * Represents the path line displayed next to the file path label on individual document pages.
+     * Also handles population of the path lines template in the correct format.
+     * 
+     * @param {string} fullPath - The full path of the currently displayed file.
+     * @param {string} tree - The tree which was searched and in which this file can be found.
+     * @param {string} icon - The icon string returned in the JSON payload.
+     */
+    
+    function buildResultHead(fullPath, tree, icon) {
+        var pathLines = '',
+            pathRoot = '/' + tree + '/source/',
+            paths = fullPath.split('/'),
+            splitPathLength = paths.length,
+            dataPath = [],
+            iconClass = icon.substring(icon.indexOf('/') + 1);
 
+        for (var pathIndex in paths) {
+            var index = parseInt(pathIndex),
+                isFirstOrOnly = index === 0 || splitPathLength === 1,
+                isLastOrOnly = (splitPathLength - 1) === index || splitPathLength === 1;
+
+            dataPath.push(paths[pathIndex]);
+
+            pathLines += nunjucks.render('path_line.html', {
+                'data_path': dataPath.join('/'),
+                'display_path': paths[pathIndex],
+                'url': pathRoot + dataPath.join('/'),
+                'is_first_or_only': isFirstOrOnly,
+                'is_dir': !isLastOrOnly
+            });
+        }
+
+        return [iconClass, pathLines];
+    }
+    
     var searchForm = $('#basic_search'),
         queryField = $('#query'),
         query = null,
@@ -334,7 +370,9 @@ $(function() {
 
             for (var result in results) {
                 var icon = results[result].icon;
-                results[result].pathLine = buildPathLine(results[result].path, data.tree, icon);
+                var resultHead = buildResultHead(results[result].path, data.tree, icon);
+                results[result].iconClass = resultHead[0];
+                results[result].pathLine = resultHead[1];
             }
 
             var container = append ? contentContainer : contentContainer.empty();
