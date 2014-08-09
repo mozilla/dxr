dafa73d4565f731e5301f97b67f06738e3abdbb5
Naveen <nave.mvn@gmail.com>
2014-08-09 00:34:34 -0500

    Add direct query capability for macros/typedefs. Fixes bug 1033677.

--- tests/test_direct.py
+++ tests/test_direct.py
@@ -9,6 +9,7 @@ from nose.tools import eq_
 
 class MemberFunctionTests(SingleFileTestCase):
     source = """
+        #define MACRO_NAME(arg1, arg2) 0
         class MemberFunction {
             public:
                 void member_function(int a);  // Don't assume the qualname
@@ -16,6 +17,7 @@ class MemberFunctionTests(SingleFileTestCase):
                                               // ().
 
             class InnerClass {
+                typedef int MyTypedef;
             };
         };
 
@@ -28,17 +30,27 @@ class MemberFunctionTests(SingleFileTestCase):
         eq_(Query(conn, query_text).direct_result(), ('main.cpp', line_num))
 
     def test_qualified_function_name_prefix(self):
-        """A unique, case-insensitive prefix match on fully qualified function
+        """An unique, case-insensitive prefix match on fully qualified function
         name should take you directly to the result."""
-        self.direct_result_eq('MemberFunction::member_FUNCTION', 12)
+        self.direct_result_eq('MemberFunction::member_FUNCTION', 14)
 
     def test_qualified_type_name(self):
-        """A unique, case-insensitive prefix match on fully qualified type name
+        """An unique, case-insensitive prefix match on fully qualified type name
         should take you directly to the result."""
-        self.direct_result_eq('MemberFunction::InnerCLASS', 8)
+        self.direct_result_eq('MemberFunction::InnerCLASS', 9)
 
     def test_line_number(self):
         """A file name and line number should take you directly to that
            file and line number."""
         self.direct_result_eq('main.cpp:6', 6)
+
+    def test_macro_name(self):
+        """An unique, case-insensitive match on a macro name should take you
+           directly to the result"""
+        self.direct_result_eq('MACRO_name', 2)
+
+    def test_typedef_name(self):
+        """An unique, case-insensitive match on a typedef name should take you
+           directly to the result"""
+        self.direct_result_eq('MyTypedef', 10)
         
