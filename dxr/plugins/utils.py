8dd1cfe7e77b3bb1576bdca9df2a6f3f3d5b9e91
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-08-19 10:24:04 -0700

    pylint + proper filter

--- dxr/plugins/utils.py
+++ dxr/plugins/utils.py
@@ -35,30 +35,29 @@ def needle_filter_factory(lang, tag, desc):
         name = tag
         domain = LINE
         description = desc
-        field_name = "{0}-{1}".format(lang, tag)
-        
+        field_name = '{0}-{1}'.format(lang, tag)
+
         def __init__(self, term):
             self.term = term
 
         def filter(self):
             return {
-                "filtered": {
-                    "term": {
-                        field_name: self.term
-                    }
-                }
-            }
+                'filtered': {
+                    'filter': {
+                        'term': {
+                            self.field_name: self.term
+                        }}}}
 
         def highlight(self, result):
             content = result['content']
+
             def _highlight(needle):
                 return needle['start'], needle['end'] or len(content)
+
             highlights = sorted((_highlight(needle) for needle
-                                 in needle[field_name]
-                                 if needle['term'] == self.term),
+                                 in content[self.field_name]
+                                 if content['term'] == self.term),
                                 key=itemgetter('start'))
             return {'content': highlights}
 
     return needle_filter_factory
-
-
