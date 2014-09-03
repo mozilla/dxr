31dc0f406c639ae5cbe8266f49404eab88c3514e
Erik Rose <erik@mozilla.com>
2014-09-03 11:48:37 -0400

    Polish syntax.
    * Reorder imports with deeper ones toward the bottom.
    * Two blank lines between top-level constructs
    * Conform docstrings to PEP 257: periods after sentences, single-sentence summary on its own line.
    * Describe *why* we're skipping in the SkipTest.
    * Wrap docstrings to 79 columns.
    * Strip trailing whitespace.

--- tests/test_direct.py
+++ tests/test_direct.py
@@ -4,17 +4,18 @@ from dxr.query import Query
 from dxr.utils import connect_db
 from dxr.testing import SingleFileTestCase, MINIMAL_MAIN
 
-from nose.tools import eq_
 from nose import SkipTest
+from nose.tools import eq_
+
 
 class DirectResultQueryTests(SingleFileTestCase):
-
     def direct_result_eq(self, query_text, line_num):
         conn = connect_db(os.path.join(self._config_dir_path, 'target', 'trees', 'code'))
         if line_num is None:
             eq_(Query(conn, query_text).direct_result(), None)
-        else:    
+        else:
             eq_(Query(conn, query_text).direct_result(), ('main.cpp', line_num))
+
 
 class MemberFunctionTests(DirectResultQueryTests):
     source = """
@@ -47,37 +48,44 @@ class MemberFunctionTests(DirectResultQueryTests):
 
     def test_qualified_function_name_prefix_sensitive(self):
         """A unique, case-sensitive prefix match on fully qualified function
-        name should take you directly to the result. This should have precedence 
-        over any case-insensitive match"""
-        raise SkipTest("Skipping test for unique case sensitive matches on function names")
+        name should take you directly to the result.
+
+        This should have precedence over any case-insensitive match.
+
+        """
+        raise SkipTest('This does not take precedence like it should.')
         self.direct_result_eq('MemberFunction::member_function', 14)
 
     def test_qualified_function_name_prefix_multiple_matches(self):
-        """Multiple prefix matches on fully qualified function 
-        name should return None"""
+        """Multiple prefix matches on fully qualified function name should
+        return None."""
         self.direct_result_eq('MemberFunction::member_FUNCTION', None)
 
     def test_qualified_type_name_insensitive(self):
-        """A unique, case-insensitive prefix match on fully qualified type 
-        name should take you directly to the result"""
+        """A unique, case-insensitive prefix match on fully qualified type
+        name should take you directly to the result."""
         self.direct_result_eq('MemberFunction::uniqueinnerClass', 12)
 
     def test_qualified_type_name_sensitive(self):
-        """A unique, case-sensitive prefix match on fully qualified type
-        name should take you directly to the result. This should have precedence 
-        over any case-insensitive match"""
-        raise SkipTest("Skipping test for unique case sensitive matches on type names")
+        """A unique, case-sensitive prefix match on fully qualified type name
+        should take you directly to the result.
+
+        This should have precedence over any case-insensitive match.
+
+        """
+        raise SkipTest('This does not take precedence like it should.')
         self.direct_result_eq('MemberFunction::InnerClass', 8)
 
     def test_qualified_type_name_multiple_matches(self):
-        """Multiple prefix matches on fully qualified type 
-        name should return None"""
+        """Multiple case-insensitive prefix matches on fully qualified type
+        name should not yield a direct result."""
         self.direct_result_eq('MemberFunction::InnerCLASS', None)
 
     def test_line_number(self):
-        """A file name and line number should take you directly to that
-        file and line number"""
+        """A file name and line number should take you directly to that file
+        and line number."""
         self.direct_result_eq('main.cpp:6', 6)
+
 
 class MacroTypedefTests(DirectResultQueryTests):
     source = """
@@ -91,41 +99,45 @@ class MacroTypedefTests(DirectResultQueryTests):
 
         #ifndef UNIQUE_MACRO_NAME
         #define UNIQUE_MACRO_NAME(arg1, arg2) 2
-        #endif        
+        #endif
 
         typedef int MyTypeDef;
         typedef int MYTYPEDEF;
         typedef int MyUniqueTypeDef;
         """ + MINIMAL_MAIN
-    
+
     def test_macro_name_insensitive(self):
-        """A unique, case-insensitive match on a macro 
-        name should take you directly to the result"""
+        """A unique, case-insensitive match on a macro name should take you
+        directly to the result."""
         self.direct_result_eq('unique_MACRO_NAME', 11)
 
     def test_macro_name_sensitive(self):
-        """A unique, case-sensitive match on a macro name should take 
-        you directly to the result. This should have precedence 
-        over any case-insensitive match"""
+        """A unique, case-sensitive match on a macro name should take you
+        directly to the result.
+
+        This should have precedence over any case-insensitive match.
+
+        """
         self.direct_result_eq('MACRO_NAME', 3)
 
     def test_macro_name_multiple_matches(self):
-        """Multiple matches on a macro 
-        name should return None"""
+        """Multiple matches on a macro name should return None."""
         self.direct_result_eq('macro_NAME', None)
 
     def test_typedef_name_insensitive(self):
-        """A unique, case-insensitive match on a typedef 
-        name should take you directly to the result"""
+        """A unique, case-insensitive match on a typedef name should take you
+        directly to the result."""
         self.direct_result_eq('myuniqueTypeDef', 16)
 
     def test_typedef_name_sensitive(self):
-        """A unique, case-sensitive match on a typedef name should take 
-        you directly to the result. This should have precedence 
-        over any case-insensitive match"""
+        """A unique, case-sensitive match on a typedef name should take you
+        directly to the result.
+
+        This should have precedence over any case-insensitive match.
+
+        """
         self.direct_result_eq('MyTypeDef', 14)
 
     def test_typedef_name_multiple_matches(self):
-        """Multiple matches on a typedef 
-        name should return None"""
-        self.direct_result_eq('myTypeDef', None)            
+        """Multiple matches on a typedef name should return None."""
+        self.direct_result_eq('myTypeDef', None)
