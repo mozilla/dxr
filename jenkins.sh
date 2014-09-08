d04eb505014120d9ab658a4ed7a7c2c271c8724e
Jamon Camisso <jamonation@gmail.com>
2014-09-08 16:28:41 -0400

    Adding some vbguest plugin detection and removing extra --provision call

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,12 @@
 git submodule sync
 git submodule update --init --recursive
 
-/opt/vagrant/bin/vagrant up --provision
+VBGUEST=$(vagrant plugin list |grep -i vbguest)
+if [ ! "$VBGUEST" ]; then
+  vagrant plugin install vagrant-vbguest
+fi;
+
+/opt/vagrant/bin/vagrant up
 /opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make clean && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
