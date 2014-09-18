56170bd7b2a0203173874af56e2f3bc57e40c412
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-09-18 16:57:11 -0500

    Added test for decode_es_datetime

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -95,5 +95,6 @@ def test_glob_to_regex():
 
 
 def test_decode_es_datetime():
+    """Test that both ES datetime formats are decoded."""
     eq_(datetime(1992, 6, 27, 0, 0), decode_es_datetime("1992-06-27T00:00:00"))
     eq_(datetime(1992, 6, 27, 0, 0, 0), decode_es_datetime("1992-06-27T00:00:00.0"))
