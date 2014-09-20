2b34fe0c135f57282736626507cad8a509ac7207
Jamon Camisso <jamonation@gmail.com>
2014-09-20 17:30:32 -0400

    Removing virtualbox build components

--- jenkins.sh
+++ jenkins.sh
@@ -11,7 +11,7 @@ fi;
 
 /opt/vagrant/bin/vagrant up
 /opt/vagrant/bin/vagrant ssh -c 'sudo adduser vagrant docker'
-/opt/vagrant/bin/vagrant ssh -c 'docker build .; docker run -i -t -v $PWD:/dxr $(docker images -q |head -n 1) /bin/bash /dxr/build.sh'
+/opt/vagrant/bin/vagrant ssh -c 'cd dxr; docker build .; docker run -i -t -v $PWD:/dxr $(docker images -q |head -n 1) /bin/bash /dxr/build.sh'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 exit $RESULT
