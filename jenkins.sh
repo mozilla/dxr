6fd160e9334e0c41eced90189f1764c45ce16016
Jamon Camisso <jamonation@gmail.com>
2014-09-20 17:52:59 -0400

    forcing use of vanilla vagrant image & running separate docker run command

--- jenkins.sh
+++ jenkins.sh
@@ -9,9 +9,12 @@ if [ ! "$VBGUEST" ]; then
   vagrant plugin install vagrant-vbguest
 fi;
 
+/opt/vagrant/bin/vagrant box remove ubuntu/trusty/64
+
 /opt/vagrant/bin/vagrant up
 /opt/vagrant/bin/vagrant ssh -c 'sudo adduser vagrant docker'
-/opt/vagrant/bin/vagrant ssh -c 'cd dxr; docker build .; docker run -i -t -v $PWD:/dxr $(docker images -q |head -n 1) /bin/bash /dxr/build.sh'
+/opt/vagrant/bin/vagrant ssh -c 'cd dxr; docker build .'
+/opt/vagrant/bin/vagrant ssh -c 'cd dxr; docker run -i -t -v $PWD:/dxr $(docker images -q |head -n 1) /bin/bash /dxr/build.sh'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 exit $RESULT
