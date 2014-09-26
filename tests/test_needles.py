41448d3de57a3516b3b4070aead8946d2b704e9f
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-09-26 17:05:09 -0500

    fixed needle tests for function needles

--- tests/test_needles.py
+++ tests/test_needles.py
@@ -5,9 +5,13 @@ from dxr.plugins.needles import (unsparsify, by_line, group_needles,
                                  span_to_lines, pack)
 from dxr.plugins.utils import Extent, Position
 
-NEEDLE1 = ('x', Extent(Position(None, 0, 3), Position(None, 0, 7)))
-NEEDLE2 = ('y', Extent(Position(None, 0, 5), Position(None, 2, 7)))
-NEEDLE3 = ('z', Extent(Position(None, 0, 0), Position(None, -1, 0)))
+KV1 = ('x', 'v1')
+KV2 = ('y', 'v2')
+KV3 = ('z', 'v3')
+
+NEEDLE1 = (KV1, Extent(Position(None, 1, 3), Position(None, 1, 7)))
+NEEDLE2 = (KV2, Extent(Position(None, 1, 5), Position(None, 3, 7)))
+NEEDLE3 = (KV3, Extent(Position(None, 1, 0), Position(None, 0, 0)))
 
 
 def eq__(lis1, lis2):
@@ -21,9 +25,9 @@ def test_needle_smoke_test():
 def test_unsparsify():
     assert_raises(ValueError, unsparsify, [NEEDLE3])
 
-    output = [[pack('x', 3, 7), pack('y', 5, None)],
-              [pack('y', 0, None)],
-              [pack('y', 0, 7)]]
+    output = [[pack(KV1, 3, 7), pack(KV2, 5, None)],
+              [pack(KV2, 0, None)],
+              [pack(KV2, 0, 7)]]
 
     eq__(output, unsparsify([NEEDLE1, NEEDLE2]))
 
@@ -31,28 +35,27 @@ def test_unsparsify():
 def test_group_needles():
     eq__([], group_needles([]))
 
-    fixture = [(pack('x', 3, 7), 0), (pack('y', 5, None), 0), (pack('y', 0, None), 1),
-               (pack('y', 0, 7), 2)]
+    fixture = [(pack(KV1, 3, 7), 1), (pack(KV2, 5, None), 1), (pack(KV2, 0, None), 2),
+               (pack(KV2, 0, 7), 3)]
 
-    output = [[pack('x', 3, 7), pack('y', 5, None)],
-              [pack('y', 0, None)],
-              [pack('y', 0, 7)]]
+    output = [[pack(KV1, 3, 7), pack(KV2, 5, None)],
+              [pack(KV2, 0, None)],
+              [pack(KV2, 0, 7)]]
     eq__(output, group_needles(fixture))
 
 
 def test_by_line():
     eq__([], by_line([]))
-    eq__([(pack('x', 3, 7), 0),
-          (pack('y', 5, None), 0),
-          (pack('y', 0, None), 1),
-          (pack('y', 0, 7), 2)],
+    eq__([(pack(KV1, 3, 7), 1),
+          (pack(KV2, 5, None), 1),
+          (pack(KV2, 0, None), 2),
+          (pack(KV2, 0, 7), 3)],
          by_line([NEEDLE1, NEEDLE2]))
 
 
 def test_span_to_lines():
-    eq__(span_to_lines(NEEDLE1), [({'term': 'x', 'start': 3, 'end': 7}, 0)])
-    eq__(span_to_lines(NEEDLE2), [({'term': 'y', 'start': 5, 'end': None}, 0),
-                                  ({'term': 'y', 'start': 0, 'end': None}, 1),
-                                  ({'term': 'y', 'start': 0, 'end': 7}, 2)])
+    eq__(span_to_lines(NEEDLE1), [(('x', {'value': 'v1', 'start': 3, 'end': 7}), 1)])
+    eq__(span_to_lines(NEEDLE2), [(('y', {'value': 'v2', 'start': 5, 'end': None}), 1),
+                                  (('y', {'value': 'v2', 'start': 0, 'end': None}), 2),
+                                  (('y', {'value': 'v2', 'start': 0, 'end': 7}), 3)])
     assert_raises(ValueError, lambda x: list(span_to_lines(x)), [])
-
