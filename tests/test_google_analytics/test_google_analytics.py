81a0c39e7b0c6c5d155901515e79a5033261623a
Venkatakrishnan Ganesh <superpulse.x@gmail.com>
2014-09-27 22:56:02 +0530

    * Fix test names and doc strings

--- tests/test_google_analytics/test_google_analytics.py
+++ tests/test_google_analytics/test_google_analytics.py
@@ -1,4 +1,4 @@
-"""Tests for emission of high-level markup, as by templates"""
+"""Tests for google analytics snippets"""
 
 from dxr.testing import DxrInstanceTestCase, run
 
@@ -6,9 +6,8 @@ from nose.tools import eq_, ok_
 
 
 class GoogleAnalyticsMarkupTests(DxrInstanceTestCase):
-    def test_autofocus_root(self):
-        """Autofocus the query field at the root of each tree but not
-        elsewhere."""
+    def test_google_analytics_snippet_exists(self):
+        """Make sure google analytics snippet does show up"""
         response = self.client().get('/code/source/')
         ok_('RANDOM_KEY_$#$' in response.data)
         ok_( '.google-analytics.com' in response.data )
