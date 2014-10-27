49d89e618253e75f764a1699af7574da67edc7a7
Erik Rose <erik@mozilla.com>
2014-10-27 12:16:01 -0400

    Unskip test_cross_file_callers.
    We don't have a problem with cross-file calls anymore, since we enhanced the compiler plugin to output both caller and callee info.

--- dxr/plugins/clang/tests/test_cross_file_callers/test_cross_file_callers.py
+++ dxr/plugins/clang/tests/test_cross_file_callers/test_cross_file_callers.py
@@ -6,14 +6,11 @@ but we don't anymore, so we can probably delete this at some point.
 """
 from dxr.testing import DxrInstanceTestCase
 
-from nose import SkipTest
-
 
 class CrossFileCallerTests(DxrInstanceTestCase):
     def test_callers(self):
         """Make sure a "caller" needle is laid down at the callsite, pointing
         to the called function."""
-        raise SkipTest('Inter-file call lookup table is not yet implemented.')
         self.found_line_eq('callers:another_file',
-                           u'int <b>main</b>(int argc, char* argv[]) {',
-                           3)
+                           u'int a = <b>another_file</b>();',
+                           4)
