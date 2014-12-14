0bda5edf5b95fbdf2566b94ca63d58547b2a52c1
Chris Greene <archgoon@gmail.com>
2014-12-14 13:06:58 -0800

    BUGZILLA-1111383 Don't assume that 'kind' is a valid key
    'kind' is not necessarily emitted during a declDef record creation,
    so we should provide a default value of None to avoid crashing.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1111383

--- dxr/plugins/clang/needles.py
+++ dxr/plugins/clang/needles.py
@@ -39,7 +39,7 @@ def needles(condensed, name, suffix='', kind=None, subkind=None, keys=('name', '
 
     """
     kind = kind or name
-    matches_subkind = (lambda entity: entity['kind'] == subkind if subkind
+    matches_subkind = (lambda entity: entity.get('kind') == subkind if subkind
                        else lambda entity: True)
     return (('c_{0}{1}'.format(name, suffix),
              dict((k, entity[k]) for k in keys),
