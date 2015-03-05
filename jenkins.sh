179469cf66adff5fb2ac27f0a36d9d32b1f07e83
Erik Rose <erik@mozilla.com>
2015-03-05 16:29:47 -0500

    Activate the venv before running tests on Jenkins. Fixes bug 1140013.

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,7 @@ if [ ! "$VBGUEST" ]; then
 fi;
 
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && make clean && make test'
+/opt/vagrant/bin/vagrant ssh -c 'cd /home/vagrant/dxr && source /home/vagrant/venv/bin/activate && make clean && make test'
 RESULT=$?
 /opt/vagrant/bin/vagrant destroy --force || exit $?
 exit $RESULT
