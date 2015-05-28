c79f09dfbeeb9c824eec2d53b68a034f3670fbe8
Peter Elmers <peter.elmers@yahoo.com>
2015-05-28 16:34:10 -0700

    Fix docstrings in tests.
    Copy paste laziness.

--- tests/test_vcs_git/test_vcs_git.py
+++ tests/test_vcs_git/test_vcs_git.py
@@ -8,35 +8,35 @@ from nose.tools import ok_
 
 
 class GitTests(DxrInstanceTestCase):
-    """Tests that the diff links for files go somewhere helpful"""
+    """Test our Git integration, both core and omniglot."""
 
     def test_diff(self):
-        """Make sure the diff link goes to the first after-initial commit."""
+        """Make sure the diff link exists and goes to the right place."""
         response = self.client().get('/code/source/main.c')
         ok_('/commit/cb339834998124cb8165aa35ed4635c51b6ac5c2" title="Diff" class="diff icon">Diff</a>' in response.data)
 
     def test_blame(self):
-        """Make sure the diff link goes to the first after-initial commit."""
+        """Make sure the blame link exists and goes to the right place."""
         response = self.client().get('/code/source/main.c')
         ok_('/blame/cb339834998124cb8165aa35ed4635c51b6ac5c2/main.c" title="Blame" class="blame icon">Blame</a>' in response.data)
 
     def test_raw(self):
-        """Make sure the diff link goes to the second after-initial commit."""
+        """Make sure the raw link exists and goes to the right place."""
         response = self.client().get('/code/source/main.c')
         ok_('/raw/cb339834998124cb8165aa35ed4635c51b6ac5c2/main.c" title="Raw" class="raw icon">Raw</a>' in response.data)
 
     def test_log(self):
-        """Make sure the diff link goes to the third after-initial commit."""
+        """Make sure the log link exists and goes to the right place."""
         response = self.client().get('/code/source/main.c')
         ok_('/commits/cb339834998124cb8165aa35ed4635c51b6ac5c2/main.c" title="Log" class="log icon">Log</a>' in response.data)
 
     def test_permalink(self):
-        """Make sure the diff link goes to the third after-initial commit."""
+        """Make sure the permalink link exists and goes to the right place."""
         # Flask's url_for will escape the url, so spaces become %20
         response = self.client().get('/code/source/main.c')
         ok_('/rev/cb339834998124cb8165aa35ed4635c51b6ac5c2/main.c" title="Permalink" class="permalink icon">Permalink</a>' in response.data)
 
     def test_pygmentize(self):
-        # Check that the pygmentize FileToSkim correctly colors a file from permalink
+        """Check that the pygmentize FileToSkim correctly colors a file from permalink."""
         response = self.client().get('/code/rev/cb339834998124cb8165aa35ed4635c51b6ac5c2/main.c')
         ok_('<span class="c">// Hello World Example</span>' in response.data)
--- tests/test_vcs_hg/test_vcs_hg.py
+++ tests/test_vcs_hg/test_vcs_hg.py
@@ -8,7 +8,7 @@ from nose.tools import ok_
 
 
 class MercurialTests(DxrInstanceTestCase):
-    """Tests that the diff links for files go somewhere helpful"""
+    """Test our Mercurial integration, both core and omniglot."""
 
     @classmethod
     def teardown_class(cls):
@@ -39,22 +39,24 @@ class MercurialTests(DxrInstanceTestCase):
         ok_('/diff/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/Filename With Space" title="Diff" class="diff icon">Diff</a>' in response.data)
 
     def test_blame(self):
-        """Make sure the diff link goes to the first after-initial commit."""
+        """Make sure the blame link goes to the right place."""
         response = self.client().get('/code/source/ChangedInCommit1')
         ok_('/annotate/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/ChangedInCommit1" title="Blame" class="blame icon">Blame</a>' in response.data)
 
     def test_raw(self):
-        """Make sure the diff link goes to the second after-initial commit."""
+        """Make sure the raw link goes to the right place."""
         response = self.client().get('/code/source/ChangedInCommit2')
         ok_('/raw-file/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/ChangedInCommit2" title="Raw" class="raw icon">Raw</a>' in response.data)
 
     def test_log(self):
-        """Make sure the diff link goes to the third after-initial commit."""
+        """Make sure the log link goes to the right place."""
         response = self.client().get('/code/source/Filename With Space')
         ok_('/filelog/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/Filename With Space" title="Log" class="log icon">Log</a>' in response.data)
 
     def test_permalink(self):
-        """Make sure the diff link goes to the third after-initial commit."""
+        """Make sure the permalink exists, and that the response is ok."""
         # Flask's url_for will escape the url, so spaces become %20
         response = self.client().get('/code/source/Filename With Space')
         ok_('/rev/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/Filename%20With%20Space" title="Permalink" class="permalink icon">Permalink</a>' in response.data)
+        response = self.client().get('/code/rev/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/Filename With Space')
+        ok_(response.status_code, 200)
