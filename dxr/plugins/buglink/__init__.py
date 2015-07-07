478fd15f451626ad91ef4cc647973bf5f1e3697e
Erik Rose <erik@mozilla.com>
2015-07-07 17:06:53 -0400

    Bring buglink and urllink plugins up to date.
    That should cover all the plugins except rust.

--- dxr/plugins/buglink/__init__.py
+++ dxr/plugins/buglink/__init__.py
@@ -6,6 +6,7 @@ from schema import Optional, Use
 
 import dxr.indexers
 from dxr.lines import Ref
+from dxr.menus import SingleDatumMenuMaker
 from dxr.plugins import Plugin, AdHocTreeToIndex
 
 
@@ -13,16 +14,30 @@ class FileToIndex(dxr.indexers.FileToIndex):
     def refs(self):
         for m in self.plugin_config.regex.finditer(self.contents):
             bug = m.group(1)
-            yield m.start(0), m.end(0), Ref([{
-                'html': cgi.escape("Bug %s" % bug),
-                'title': "Find this bug in %s" % self.plugin_config.name,
-                'href': self.plugin_config.url % bug,
-                'icon': 'buglink'}])
+            yield m.start(0), m.end(0), Ref(
+                # We could make this more storage-efficient if we gave
+                # menumakers access to the plugin config at request time.
+                [BugMenuMaker(self.tree,
+                              (self.plugin_config.name,
+                               self.plugin_config.url,
+                               bug))])
+
+
+class BugMenuMaker(SingleDatumMenuMaker):
+    plugin = 'buglink'
+
+    def menu_items(self):
+        name, url, bug = self.data
+        yield {'html': cgi.escape("Bug %s" % bug),
+               'title': "Find this bug in %s" % name,
+               'href': url % bug,
+               'icon': 'buglink'}
 
 
 plugin = Plugin(
         tree_to_index=partial(AdHocTreeToIndex,
                               file_to_index_class=FileToIndex),
+        menus=[BugMenuMaker],
         config_schema={
             'url': str,
             Optional('name', default='the bug tracker'): basestring,
--- dxr/plugins/urllink.py
+++ dxr/plugins/urllink.py
@@ -2,6 +2,7 @@ import re
 
 import dxr.indexers
 from dxr.lines import Ref
+from dxr.menus import SingleDatumMenuMaker
 
 
 # From http://stackoverflow.com/a/1547940
@@ -12,8 +13,14 @@ class FileToIndex(dxr.indexers.FileToIndex):
     def refs(self):
         for m in url_re.finditer(self.contents):
             url = m.group(0)
-            yield m.start(0), m.end(0), Ref([{
-                'html': 'Follow link',
-                'title': 'Visit %s' % url,
-                'href': url,
-                'icon': 'external_link'}])
+            yield m.start(0), m.end(0), Ref([UrlMenuMaker(self.tree, url)])
+
+
+class UrlMenuMaker(SingleDatumMenuMaker):
+    plugin = 'urllink'
+
+    def menu_items(self):
+        yield {'html': 'Follow link',
+               'title': 'Visit %s' % self.data,
+               'href': self.data,
+               'icon': 'external_link'}
