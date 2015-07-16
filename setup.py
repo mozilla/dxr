234f58937d876c3d6e7bab546d4f0501497a70b7
Peter Elmers <peter.elmers@yahoo.com>
2015-07-16 10:50:27 -0700

    Add ply to requirements.txt and xpidl to install_requires.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
                       'pyelasticsearch>=1.1,<2.0',
                       'Pygments>=2.0.1,<3.0',
                       'python-hglib>=1.6,<2.0',
+                      'xpidl>=1.0',
                       'requests>=1.0,<2.0'],
     tests_require=['nose>=1.3.0,<2.0'],
     test_suite='nose.collector',
