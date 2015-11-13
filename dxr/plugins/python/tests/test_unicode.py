5cbd00eb779600af29549895d8594128915c4b7d
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-11-13 16:11:50 -0500

    Made the test comments into docstrings.

--- dxr/plugins/python/tests/test_unicode.py
+++ dxr/plugins/python/tests/test_unicode.py
@@ -21,11 +21,12 @@ class EncodingCommentTests(PythonSingleFileTestCase):
     source = u'# -*- coding: utf-8 -*-\ndef foo():\n    return 42'
 
     def test_encoding_comment_files_are_not_skipped(self):
-        # Calling ast.parse on unicode objects that have an encoding comment
-        # in the first two lines is subject to a bug in Python 2.  See
-        # http://bugs.python.org/issue22221.  Show that we can process such a
-        # file without skipping or error.
+        """Calling ast.parse on unicode objects that have an encoding comment
+        in the first two lines is subject to a bug in Python 2.  See
+        http://bugs.python.org/issue22221.  Show that we can process
+        such a file without skipping or error.
 
+        """
         self.found_line_eq('function:foo', "def <b>foo</b>():", 2)
 
 
@@ -42,7 +43,8 @@ class UnicodeOffsetTests(PythonSingleFileTestCase):
     """)
 
     def test_call_offsets(self):
-        # Check that the offset of the function call is calculated in
-        # unicode characters, not bytes.
+        """Check that the offset of the function call is calculated in unicode
+        characters, not bytes.
 
+        """
         self.found_line_eq('callers:kilroy', u'print u"どうもありがとう " + <b>kilroy()</b>', 9)
