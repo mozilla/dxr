97f7cccc03082ac624458aeb7d9b717de6d2b2b5
Tom Klein <twointofive@gmail.com>
2016-03-10 16:46:13 -0600

    Additional testing for VCSes.

--- tests/test_vcs_git/test_vcs_git.py
+++ tests/test_vcs_git/test_vcs_git.py
@@ -1,41 +1,30 @@
-from nose.tools import ok_, eq_
+from nose.tools import ok_, eq_, raises
 
 from dxr.testing import DxrInstanceTestCaseMakeFirst
 
-LATEST_REVISION = "5e2b2b554eb86f90e189217fa9dc2eba66259910"
-PREVIOUS_REVISION = "cb339834998124cb8165aa35ed4635c51b6ac5c2"
+GH_REPO = "https://github.com/someone/somerepo"
+
+# Github remote Https://github.com:
+LATEST_G_H_REVISION = "5e2b2b554eb86f90e189217fa9dc2eba66259910"
+PREVIOUS_G_H_REVISION = "cb339834998124cb8165aa35ed4635c51b6ac5c2"
+
+# Github remote Ssh://user@github.com:port
+LATEST_G_S_REVISION = "f649b19200758ccf0592829abf03b03c4fa2182d"
+
+BB_REPO = "https://bitbucket.org/someone/somerepo"
+# Bitbucket remote Git@bitbucket.org:
+LATEST_B_G_REVISION = "2d45655982e5267b6c1ee6173aac14be89311947"
 
 class GitTests(DxrInstanceTestCaseMakeFirst):
     """Test our Git integration, both core and omniglot."""
-
-    def test_diff(self):
-        """Make sure the diff link exists and goes to the right place."""
-        response = self.client().get('/code/source/main.c')
-        ok_('/commit/%s" title="Diff" class="diff icon">Diff</a>' % LATEST_REVISION in response.data)
-
-    def test_blame(self):
-        """Make sure the blame link exists and goes to the right place."""
-        response = self.client().get('/code/source/main.c')
-        ok_('/blame/%s/main.c" title="Blame" class="blame icon">Blame</a>' % LATEST_REVISION in response.data)
-
-    def test_raw(self):
-        """Make sure the raw link exists and goes to the right place."""
-        response = self.client().get('/code/source/main.c')
-        ok_('/raw/%s/main.c" title="Raw" class="raw icon">Raw</a>' % LATEST_REVISION in response.data)
-
-    def test_log(self):
-        """Make sure the log link exists and goes to the right place."""
-        response = self.client().get('/code/source/main.c')
-        ok_('/commits/%s/main.c" title="Log" class="log icon">Log</a>' % LATEST_REVISION in response.data)
-
     def test_permalink(self):
         """Make sure the permalink link exists and goes to the right place."""
-        response = self.client().get('/code/source/main.c')
-        ok_('/rev/%s/main.c" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_REVISION in response.data)
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('/rev/%s/github_https_repo/main.c" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_G_H_REVISION in response.data)
         # Test that it works for this revision and the last one.
-        response = self.client().get('/code/rev/%s/main.c' % LATEST_REVISION)
+        response = self.client().get('/code/rev/%s/github_https_repo/main.c' % LATEST_G_H_REVISION)
         eq_(response.status_code, 200)
-        response = self.client().get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = self.client().get('/code/rev/%s/github_https_repo/main.c' % PREVIOUS_G_H_REVISION)
         eq_(response.status_code, 200)
 
     def test_deep_permalink(self):
@@ -44,17 +33,86 @@ class GitTests(DxrInstanceTestCaseMakeFirst):
         the top level git root directory, since `git show` will resolve paths relative to the git
         root rather than the current working directory unless we specify ./ before the path."""
 
-        response = self.client().get('/code/source/deeper/deeper_file')
-        ok_('/rev/%s/deeper/deeper_file" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_REVISION in response.data)
-        response = self.client().get('/code/rev/%s/deeper/deeper_file' % LATEST_REVISION)
+        response = self.client().get('/code/source/github_https_repo/deeper/deeper_file')
+        ok_('/rev/%s/github_https_repo/deeper/deeper_file" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_G_H_REVISION in response.data)
+        response = self.client().get('/code/rev/%s/github_https_repo/deeper/deeper_file' % LATEST_G_H_REVISION)
         eq_(response.status_code, 200)
         ok_("This file tests" in response.data)
 
     def test_pygmentize(self):
         """Check that the pygmentize FileToSkim correctly colors a file from permalink."""
         client = self.client()
-        response = client.get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = client.get('/code/rev/%s/github_https_repo/main.c' % PREVIOUS_G_H_REVISION)
         ok_('<span class="c">// Hello World Example\n</span>' in response.data)
         # Query it again to test that the Vcs cache functions.
-        response = client.get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = client.get('/code/rev/%s/github_https_repo/main.c' % PREVIOUS_G_H_REVISION)
         ok_('<span class="c">// Hello World Example\n</span>' in response.data)
+
+    @raises(AssertionError) # remove this line when fixed
+    def test_untracked_file(self):
+        """Check that the sidebar displays "Untracked file" if and only if a
+        file is untracked."""
+        response = self.client().get('/code/source/untracked_file')
+        ok_('<h4>Untracked file</h4>' in response.data)
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('<h4>Untracked file</h4>' not in response.data)
+
+    def test_vcs_links_display(self):
+        """Check that the "VCS Links" options appear if and only if we're able
+        to find a suitable remote for a file in a VCS repository."""
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('<h4>VCS Links</h4>' in response.data)
+        # no_remote/README is tracked, but its repo has no remote.
+        response = self.client().get('/code/source/no_remote/README')
+        ok_('<h4>VCS Links</h4>' not in response.data)
+
+    #### Github "origin" remote https://github.com/someone/somerepo.git
+    def test_github_https_diff(self):
+        """Make sure the github diff link exists and goes to the right place."""
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('%s/commit/%s" title="Diff" class="diff icon">Diff</a>' % (GH_REPO, LATEST_G_H_REVISION) in response.data)
+
+    def test_github_https_blame(self):
+        """Make sure the github blame link exists and goes to the right place."""
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('%s/blame/%s/main.c" title="Blame" class="blame icon">Blame</a>' % (GH_REPO, LATEST_G_H_REVISION) in response.data)
+
+    def test_github_https_raw(self):
+        """Make sure the github raw link exists and goes to the right place."""
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('%s/raw/%s/main.c" title="Raw" class="raw icon">Raw</a>' % (GH_REPO, LATEST_G_H_REVISION) in response.data)
+
+    def test_github_https_log(self):
+        """Make sure the github log link exists and goes to the right place."""
+        response = self.client().get('/code/source/github_https_repo/main.c')
+        ok_('%s/commits/%s/main.c" title="Log" class="log icon">Log</a>' % (GH_REPO, LATEST_G_H_REVISION) in response.data)
+
+    #### Github "origin" remote ssh://user@github.com:1111/someone/somerepo.git
+    def test_repo_root_outside_code(self):
+        """Make sure we find a repo even when its root is outside our tree root."""
+        response = self.client().get('/code/source/in_repo_outside_code')
+        ok_('https://github.com:1111/someone/somerepo/commits/%s/code/in_repo_outside_code" title="Log" class="log icon">Log</a>' % LATEST_G_S_REVISION in response.data)
+
+    #### Bitbucket "origin" remote git@bitbucket.org:someone/somerepo.git
+    def test_bitbucket_gitssh_diff(self):
+        """Make sure the bitbucket diff link exists and goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_gitssh_repo/README')
+        ok_('%s/diff/README?diff2=%s" title="Diff" class="diff icon">Diff</a>' % (BB_REPO, LATEST_B_G_REVISION) in response.data)
+
+    def test_bitbucket_gitssh_blame(self):
+        """Make sure the bitbucket blame link exists and goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_gitssh_repo/README')
+        ok_('%s/annotate/%s/README" title="Blame" class="blame icon">Blame</a>' % (BB_REPO, LATEST_B_G_REVISION) in response.data)
+
+    def test_bitbucket_gitssh_raw(self):
+        """Make sure the bitbucket raw link exists and goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_gitssh_repo/README')
+        ok_('%s/raw/%s/README" title="Raw" class="raw icon">Raw</a>' % (BB_REPO, LATEST_B_G_REVISION) in response.data)
+
+    def test_bitbucket_gitssh_log(self):
+        """Make sure the bitbucket log link exists and goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_gitssh_repo/README')
+        ok_('%s/history-node/%s/README" title="Log" class="log icon">Log</a>' % (BB_REPO, LATEST_B_G_REVISION) in response.data)
+
+    #### There's also the repo code/no_remote which has no remote - if we don't
+    #### assert during indexing then we pass.
--- tests/test_vcs_hg/test_vcs_hg.py
+++ tests/test_vcs_hg/test_vcs_hg.py
@@ -6,44 +6,81 @@ from dxr.testing import DxrInstanceTestCaseMakeFirst
 from nose import SkipTest
 from nose.tools import ok_
 
+# Hgweb remote http://33.33.33.77:8001
+H_REPO = 'http://33.33.33.77:8001'
+SECOND_H_REVISION = '2e86c4e11a82f3ec17867468e499e85ec3cbf441'
+THIRD_H_REVISION = 'cd18424a4dab95361e25e86398e557d3d889e2c8'
+FOURTH_H_REVISION = '1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc'
+FIFTH_H_REVISION = '84798105c9ab5897f8c7d630d133d9003b44a62f'
+
+# Bitbucket remote ssh://user@bitbucket.org
+BS_REPO = 'https://bitbucket.org/someone/somerepo'
+FIRST_BS_REVISION = 'a7eb0f0153637af80073fc3c395524fb734fa535'
+
 
 class MercurialTests(DxrInstanceTestCaseMakeFirst):
     """Test our Mercurial integration, both core and omniglot."""
-
-    def test_diff_file1(self):
-        """Make sure the diff link goes to the first after-initial commit."""
-        response = self.client().get('/code/source/ChangedInCommit1')
-        ok_('/diff/2e86c4e11a82f3ec17867468e499e85ec3cbf441/ChangedInCommit1" title="Diff" class="diff icon">Diff</a>' in response.data)
-
-    def test_diff_file2(self):
-        """Make sure the diff link goes to the second after-initial commit."""
-        response = self.client().get('/code/source/ChangedInCommit2')
-        ok_('/diff/cd18424a4dab95361e25e86398e557d3d889e2c8/ChangedInCommit2" title="Diff" class="diff icon">Diff</a>' in response.data)
-
-    def test_diff_file3(self):
-        """Make sure the diff link goes to the third after-initial commit."""
-        response = self.client().get('/code/source/Filename With Space')
-        ok_('/diff/1be3fc90ef0104cf186fac7bc0bbfea17ba6ebdc/Filename With Space" title="Diff" class="diff icon">Diff</a>' in response.data)
-
-    def test_blame(self):
-        """Make sure the blame link goes to the right place."""
-        response = self.client().get('/code/source/ChangedInCommit1')
-        ok_('/annotate/84798105c9ab5897f8c7d630d133d9003b44a62f/ChangedInCommit1" title="Blame" class="blame icon">Blame</a>' in response.data)
-
-    def test_raw(self):
-        """Make sure the raw link goes to the right place."""
-        response = self.client().get('/code/source/ChangedInCommit2')
-        ok_('/raw-file/84798105c9ab5897f8c7d630d133d9003b44a62f/ChangedInCommit2" title="Raw" class="raw icon">Raw</a>' in response.data)
-
-    def test_log(self):
-        """Make sure the log link goes to the right place."""
-        response = self.client().get('/code/source/Filename With Space')
-        ok_('/filelog/84798105c9ab5897f8c7d630d133d9003b44a62f/Filename With Space" title="Log" class="log icon">Log</a>' in response.data)
-
     def test_permalink(self):
         """Make sure the permalink exists, and that the response is ok."""
         # Flask's url_for will escape the url, so spaces become %20
-        response = self.client().get('/code/source/Colon: name')
-        ok_('/rev/84798105c9ab5897f8c7d630d133d9003b44a62f/Colon:%20name" title="Permalink" class="permalink icon">Permalink</a>' in response.data)
-        response = self.client().get('/code/rev/84798105c9ab5897f8c7d630d133d9003b44a62f/Colon: name')
+        response = self.client().get('/code/source/ip_addr_repo/Colon: name')
+        ok_('/rev/%s/ip_addr_repo/Colon:%%20name" title="Permalink" class="permalink icon">Permalink</a>' % FIFTH_H_REVISION in response.data)
+        response = self.client().get('/code/rev/%s/Colon: name' % FIFTH_H_REVISION)
         ok_(response.status_code, 200)
+
+    #### Hgweb 'default' remote http://33.33.33.77:8001 :
+    def test_hgweb_diff_file1(self):
+        """Make sure the diff link goes to the first after-initial commit."""
+        response = self.client().get('/code/source/ip_addr_repo/ChangedInCommit1')
+        ok_('%s/diff/%s/ChangedInCommit1" title="Diff" class="diff icon">Diff</a>' % (H_REPO, SECOND_H_REVISION) in response.data)
+
+    def test_hgweb_diff_file2(self):
+        """Make sure the diff link goes to the second after-initial commit."""
+        response = self.client().get('/code/source/ip_addr_repo/ChangedInCommit2')
+        ok_('%s/diff/%s/ChangedInCommit2" title="Diff" class="diff icon">Diff</a>' % (H_REPO, THIRD_H_REVISION) in response.data)
+
+    def test_hgweb_diff_file3(self):
+        """Make sure the diff link goes to the third after-initial commit."""
+        response = self.client().get('/code/source/ip_addr_repo/Filename With Space')
+        ok_('%s/diff/%s/Filename With Space" title="Diff" class="diff icon">Diff</a>' % (H_REPO, FOURTH_H_REVISION) in response.data)
+
+    def test_hgweb_blame(self):
+        """Make sure the blame link goes to the right place."""
+        response = self.client().get('/code/source/ip_addr_repo/ChangedInCommit1')
+        ok_('%s/annotate/%s/ChangedInCommit1" title="Blame" class="blame icon">Blame</a>' % (H_REPO, FIFTH_H_REVISION) in response.data)
+
+    def test_hgweb_raw(self):
+        """Make sure the raw link goes to the right place."""
+        response = self.client().get('/code/source/ip_addr_repo/ChangedInCommit2')
+        ok_('%s/raw-file/%s/ChangedInCommit2" title="Raw" class="raw icon">Raw</a>' % (H_REPO, FIFTH_H_REVISION) in response.data)
+
+    def test_hgweb_log(self):
+        """Make sure the log link goes to the right place."""
+        response = self.client().get('/code/source/ip_addr_repo/Filename With Space')
+        ok_('%s/filelog/%s/Filename With Space" title="Log" class="log icon">Log</a>' % (H_REPO, FIFTH_H_REVISION) in response.data)
+
+    #### Bitbucket 'default' remote ssh://user@bitbucket.org/someone/somerepo
+    def test_bitbucket_diff(self):
+        """Make sure the diff link goes to the one and only commit."""
+        response = self.client().get('/code/source/bitbucket_repo/README')
+        # Bitbucket looks up an implicit diff1 in which the file at rev diff2
+        # last changed, so our rev should just be the current rev.
+        ok_('%s/diff/README?diff2=%s" title="Diff" class="diff icon">Diff</a>' % (BS_REPO, FIRST_BS_REVISION) in response.data)
+
+    def test_bitbucket_blame(self):
+        """Make sure the blame link goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_repo/README')
+        ok_('%s/annotate/%s/README" title="Blame" class="blame icon">Blame</a>' % (BS_REPO, FIRST_BS_REVISION) in response.data)
+
+    def test_bitbucket_raw(self):
+        """Make sure the raw link goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_repo/README')
+        ok_('%s/raw/%s/README" title="Raw" class="raw icon">Raw</a>' % (BS_REPO, FIRST_BS_REVISION) in response.data)
+
+    def test_bitbucket_log(self):
+        """Make sure the log link goes to the right place."""
+        response = self.client().get('/code/source/bitbucket_repo/README')
+        ok_('%s/history-node/%s/README" title="Log" class="log icon">Log</a>' % (BS_REPO, FIRST_BS_REVISION) in response.data)
+
+    #### There's also the repo code/no_default with no 'default' remote - if we
+    #### don't assert during indexing then we pass.
