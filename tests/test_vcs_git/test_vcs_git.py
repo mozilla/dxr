abd20fe833d63f2c5bb81d452fba227da3c68967
Tom Klein <twointofive@gmail.com>
2016-04-04 23:15:35 -0500

    Show '(binary file)' for /source/binary, don't assert on /rev/binary.  Fixes bug 1222572.
    Also, in test_binary_files.py, don't allow redirect on the 'path:some_bytes'
    query since if/when we switch to jumping to a single result, that test would
    still pass (for the wrong reasons).

--- tests/test_vcs_git/test_vcs_git.py
+++ tests/test_vcs_git/test_vcs_git.py
@@ -2,8 +2,8 @@ from nose.tools import ok_, eq_
 
 from dxr.testing import DxrInstanceTestCaseMakeFirst
 
-LATEST_REVISION = "5e2b2b554eb86f90e189217fa9dc2eba66259910"
-PREVIOUS_REVISION = "cb339834998124cb8165aa35ed4635c51b6ac5c2"
+LATEST_REVISION = "d3c6c4da4b963aa9f34f48f5ded43ab573b2876c"
+OLDER_REVISION = "cb339834998124cb8165aa35ed4635c51b6ac5c2"
 
 class GitTests(DxrInstanceTestCaseMakeFirst):
     """Test our Git integration, both core and omniglot."""
@@ -32,11 +32,19 @@ class GitTests(DxrInstanceTestCaseMakeFirst):
         """Make sure the permalink link exists and goes to the right place."""
         response = self.client().get('/code/source/main.c')
         ok_('/rev/%s/main.c" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_REVISION in response.data)
-        # Test that it works for this revision and the last one.
+        # Test that it works for this revision and an older one.
         response = self.client().get('/code/rev/%s/main.c' % LATEST_REVISION)
         eq_(response.status_code, 200)
-        response = self.client().get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = self.client().get('/code/rev/%s/main.c' % OLDER_REVISION)
         eq_(response.status_code, 200)
+
+    def test_binary_permalink(self):
+        """Make sure the permalink link exists for a binary file, and that the
+        permalink version of binary_file shows '(binary file)'."""
+        response = self.client().get('/code/source/binary_file')
+        ok_('/rev/%s/binary_file" title="Permalink" class="permalink icon">Permalink</a>' % LATEST_REVISION in response.data)
+        response = self.client().get('/code/rev/%s/binary_file' % LATEST_REVISION)
+        ok_('(binary file)' in response.data)
 
     def test_deep_permalink(self):
         """Make sure the permalink link exists and goes to the right place for files not in the
@@ -53,8 +61,8 @@ class GitTests(DxrInstanceTestCaseMakeFirst):
     def test_pygmentize(self):
         """Check that the pygmentize FileToSkim correctly colors a file from permalink."""
         client = self.client()
-        response = client.get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = client.get('/code/rev/%s/main.c' % OLDER_REVISION)
         ok_('<span class="c">// Hello World Example\n</span>' in response.data)
         # Query it again to test that the Vcs cache functions.
-        response = client.get('/code/rev/%s/main.c' % PREVIOUS_REVISION)
+        response = client.get('/code/rev/%s/main.c' % OLDER_REVISION)
         ok_('<span class="c">// Hello World Example\n</span>' in response.data)
