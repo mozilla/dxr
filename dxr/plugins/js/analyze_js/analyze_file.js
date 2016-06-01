6849c6ec2d4239d001c64c1b40cf08d7aedc68ae
Peter Elmers <peter.elmers@yahoo.com>
2016-05-31 18:03:13 -0700

    Avoid concretizing refs and needles in js indexer, and namedtuple over dicts.
    Also use warn and error over log.

--- dxr/plugins/js/analyze_js/analyze_file.js
+++ dxr/plugins/js/analyze_js/analyze_file.js
@@ -329,7 +329,7 @@ const Analyzer = {
       break;
 
     default:
-      console.log(`In ${fileIndex}, Unexpected statement: ${stmt.type} ${JSON.stringify(stmt)}`);
+      console.warn(`In ${fileIndex}, Unexpected statement: ${stmt.type} ${JSON.stringify(stmt)}`);
       break;
     }
   },
@@ -388,7 +388,7 @@ const Analyzer = {
 
   // Handle an expression by dispatching based on its type.
   expression(expr) {
-    if (!expr) console.log(Error().stack);
+    if (!expr) console.warn(Error().stack);
 
     switch (expr.type) {
     case "Identifier":
@@ -613,8 +613,8 @@ const Analyzer = {
       break;
 
     default:
-      console.log(Error().stack);
-      console.log(`In ${fileIndex}, Unexpected expression ${expr.type}: ${JSON.stringify(expr)}`);
+      console.warn(Error().stack);
+      console.warn(`In ${fileIndex}, Unexpected expression ${expr.type}: ${JSON.stringify(expr)}`);
       break;
     }
   },
@@ -635,7 +635,7 @@ const Analyzer = {
   // Handle a pattern-matching assignment by dispatching on type.
   pattern(pat) {
     if (!pat) {
-      console.log(Error().stack);
+      console.warn(Error().stack);
     }
 
     switch (pat.type) {
@@ -671,7 +671,7 @@ const Analyzer = {
       break;
 
     default:
-      console.log(`In ${fileIndex}, Unexpected pattern: ${pat.type} ${JSON.stringify(pat)}`);
+      console.warn(`In ${fileIndex}, Unexpected pattern: ${pat.type} ${JSON.stringify(pat)}`);
       break;
     }
   }
@@ -749,7 +749,7 @@ function analyzeJS(filepath, relpath, tempFilepath)
       Analyzer.program(ast);
     }
   } catch (e) {
-    console.log(fileIndex, e.name, e.message);
+    console.error(fileIndex, e.name, e.message);
   }
   fs.writeFileSync(tempFilepath, outLines.join('\n'));
 }
