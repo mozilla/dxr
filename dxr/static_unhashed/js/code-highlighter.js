c50493aeaffb85aae75843d06422d73d50e1cad7
James Abbatiello <abbeyj at gmail.com>
2016-06-13 14:53:52 -0400

    Fix scrolling to selected line on page load in Chrome. Trying to scroll in the document ready handler in Chrome doesn't work because Chrome will reset the scroll position later.  Delaying the scroll with setTimeout works around this problem.

--- dxr/static_unhashed/js/code-highlighter.js
+++ dxr/static_unhashed/js/code-highlighter.js
@@ -316,11 +316,12 @@ $(function () {
             //for directly linked line(s), scroll to the offset minus 150px for fixed search bar height
             //but only scrollTo if the offset is more than 150px in distance from the top of the page
             jumpPosition = parseInt(jumpPosition.top, 10) - 150;
-            if (jumpPosition >= 0) {
-                window.scrollTo(0, jumpPosition);
-            } else {
-                window.scrollTo(0, 0);
+            if (jumpPosition < 0) {
+                jumpPosition = 0;
             }
+            window.setTimeout(function() {
+                window.scrollTo(0, jumpPosition);
+            }, 0);
             //tidy up an incoming url that might be typed in manually
             setWindowHash();
         }
