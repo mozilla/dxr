1996d6d4ddd024cbf31f044dae2d52b66d78fc81
Peter Elmers <peter.elmers@yahoo.com>
2016-06-24 09:17:58 -0700

    Make sure the test actually will run.

--- tests/test_non_utf8_paths/test_non_utf8_paths.py
+++ tests/test_non_utf8_paths/test_non_utf8_paths.py
@@ -3,3 +3,6 @@ from dxr.testing import DxrInstanceTestCase
 
 class NonUTF8PathTest(DxrInstanceTestCase):
     """Just tests that the index can be created without error."""
+
+    def test_indexes(self):
+        pass
