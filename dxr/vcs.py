22150fe578a36f8bfa6e832d7516326df4163b51
Claas Augner <mozilla@caugner.de>
2016-07-04 21:29:58 +0200

    vcs.py: Fix lint.

--- dxr/vcs.py
+++ dxr/vcs.py
@@ -372,7 +372,7 @@ class Subversion(Vcs):
         return path in self.files
 
     def has_upstream(self):
-        return self.upstream != None
+        return self.upstream is not None
 
     def generate_log(self, path):
         return None
