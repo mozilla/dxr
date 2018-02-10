94882fe50e76f67f1196048df666f43860713ab4
James Abbatiello <abbeyj@gmail.com>
2018-02-10 15:37:40 -0500

    Allow searching for references to things defined in third-party code.

---
+++ dxr/plugins/clang/tests/test_third_party_refs.py
@@ -0,0 +1,51 @@
+from dxr.plugins.clang.tests import CSingleFileTestCase
+
+
+class ThirdPartyRefsTests(CSingleFileTestCase):
+    source = """
+        #include <string.h>
+
+        void foo(int);
+
+        struct C {
+            C(int);
+        };
+
+        int main()
+        {
+            char s[6];
+            strcpy(s, "Hello");
+            foo(1);
+            C c(1);
+            return 0;
+        }
+        """
+
+    def test_ref_libc(self):
+        self.found_line_eq('function-ref:strcpy',
+            '<b>strcpy</b>(s, "Hello");')
+
+    def test_ref_not_defined(self):
+        self.found_line_eq('+function-ref:foo(int)',
+            '<b>foo</b>(1);')
+
+    def test_decl_not_defined(self):
+        self.found_line_eq('+function-decl:foo(int)',
+            'void <b>foo</b>(int);')
+
+    def test_decl_constructor(self):
+        self.found_line_eq('+function-decl:C::C(int)',
+            '<b>C</b>(int);')
+
+    def test_call_libc(self):
+        self.found_line_eq('callers:strcpy',
+            '<b>strcpy(s, "Hello")</b>;')
+
+    def test_call_not_defined(self):
+        self.found_line_eq('+callers:foo(int)',
+            '<b>foo(1)</b>;')
+
+    def test_call_constructor(self):
+        self.found_line_eq('+callers:C::C(int)',
+            'C <b>c(1)</b>;')
+
